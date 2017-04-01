27100d00c15f403dde969a8be3da26ee1c854ebd
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-02 00:36:58 +0300

    fix dev mode

--- webpack_config/config.js
+++ webpack_config/config.js
@@ -9,7 +9,7 @@ module.exports = {
   srcPath: path.join(__dirname, './../common'),
   // add these dependencies to a standalone vendor bundle
   vendor: [
-    'react', 'react-dom', 'react-router', 'redux', 'react-router-redux', 'redux-thunk', 'semantic-ui-react', 'whatwg-fetch'
+    'react', 'react-dom', 'react-router', 'redux', 'react-router-redux', 'redux-thunk', 'semantic-ui-react', 'whatwg-fetch', 'semantic-ui-css/semantic.css'
   ],
   // enable babelrc
   babel: {
