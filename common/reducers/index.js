9e75d5a3dbff3b07bf8980ef066bc7a34bde2e86
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-14 17:18:40 +0300

    fix travis.yml

--- common/reducers/index.js
+++ common/reducers/index.js
@@ -1,11 +1,12 @@
+import {combineReducers} from 'redux'
+import {routerReducer} from 'react-router-redux'
+
 import * as layout from './layout';
 import * as inbox from './inbox'
 import * as auth from './auth'
 import * as loginCR from './login_component_reducer'
 import * as dashboard from './dashboard'
 
-import {combineReducers} from 'redux'
-import {routerReducer} from 'react-router-redux'
 export const rootReducer = combineReducers({
     ...layout,
     ...inbox,
