97f5842f956fbfa0fc647d3bd55d7452bab63685
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-15 18:55:25 +0300

    join two "routings" into complete one routing of whole app, it includes: sidebar routing + real routing

--- common/routing/index.jsx
+++ common/routing/index.jsx
@@ -2,39 +2,59 @@ import React from 'react';
 import {Route, Redirect, Switch} from 'react-router';
 import {createBrowserHistory} from 'history'
 import {App, Inbox, Dashboard, Login} from 'containers';
+import {RouteAuth} from 'components'
 
 export const history = getHistory()
+
+export const appRouting = [
+    {
+        path: '/',
+        icon: 'newspaper',
+        name: 'Dashboard',
+        exact: true,
+        sidebarVisible: true,
+        tag: RouteAuth,
+        component: Dashboard
+    }, {
+        path: '/inbox',
+        name: 'Inbox',
+        icon: 'comments outline',
+        sidebarVisible: true,
+        tag: RouteAuth,
+        component: Inbox
+    }, {
+        external: true,
+        path: 'https://github.com/Metnew/react-semantic.ui-starter',
+        icon: 'github',
+        name: 'Github',
+        sidebarVisible: true
+    }, {
+        path: '/auth',
+        name: 'Auth',
+        tag: Route,
+        component: Login
+    }
+]
 
 /**
  * Returns application routing with protected by AuthCheck func routes
  * @param {Function} AuthCheck checks is user logged in
  */
 export const Routing = (AuthCheck) => {
-    let routes = [
-        {
-            name: 'Login',
-            path: '/auth',
-            tag: Route,
-            component: Login
-        }, {
-            name: 'Dashboard',
-            path: '/',
-            tag: Route,
-            component: Dashboard,
-            exact: true
-        }, {
-            name: 'Inbox',
-            path: '/inbox',
-            tag: Route,
-            component: Inbox
+    // remove components that aren't application routes, (e.g. github link in sidebar)
+    let routes = appRouting.filter(a => a.tag || a.component).map(a => {
+        let {tag, path, exact, component} = a
+        let b = {
+            tag,
+            path,
+            exact,
+            component
         }
-    ]
-
-    let rot = routes
-    // .filter((a) => {
-    //     return AuthCheck(a.path)
-    // })
-        .map((a) => {
+        return b
+    })
+    // render components that are inside Switch (main view)
+    let routesRendered = routes.map((a, i) => {
+        // get tag for Route. is it RouteAuth `protected route` or Route?
         let Tag = a.tag
         delete a.tag
         return (<Tag key={Math.random()} {...a}/>)
