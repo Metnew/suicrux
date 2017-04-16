11c3ec18701e371bf73077caef11c9c10112c29f
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-16 12:59:24 +0300

    remove posts with userId=2 from getStatistics request

--- common/api/StatisticsSvc.js
+++ common/api/StatisticsSvc.js
@@ -1,5 +1,5 @@
 import {get} from './utils'
 
 export async function getStatistics_API() {
-	return await get('https://jsonplaceholder.typicode.com/posts?userId=1&userId=2')
+	return await get('https://jsonplaceholder.typicode.com/posts?userId=1')
 }
