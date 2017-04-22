a10c8028d28338dee898beba5d00529fd3e4a8fe
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-22 13:30:49 +0300

    move Logo.jsx to own folder

---
+++ common/components/Logo/index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import {Image} from 'semantic-ui-react'
+
+class Logo extends Component {
+	shouldComponentUpdate () {
+		return false
+	}
+
+	render () {
+		let url = './images/logo.png'
+		return (
+			<div className="logo">
+				<Image src={url} centered height="34px" />
+			</div>
+		)
+	}
+}
+
+export default Logo
