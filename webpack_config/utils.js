a02cfa0a619b5234ca7f0c1e40b395c49901cac6
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-23 14:37:49 +0300

    remove unneccessary _.loaderOptions()

--- webpack_config/utils.js
+++ webpack_config/utils.js
@@ -13,16 +13,3 @@ _.outputPath = path.join(__dirname, '../dist')
 _.outputIndexPath = path.join(__dirname, '../dist/index.html')
 
 _.target = 'web'
-
-_.loadersOptions = () => {
-	const isProd = process.env.NODE_ENV === 'production'
-
-	return {
-		minimize: isProd,
-		options: {
-			// css-loader relies on context
-			context: process.cwd(),
-			babel: config.babel
-		}
-	}
-}
