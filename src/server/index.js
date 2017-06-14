6fb20725e79ff7657b066529520df247191d4165
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-06-14 16:38:11 +0300

    formatting

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 require('babel-core/register')
-require.extensions['.scss'] = () => {}
-require.extensions['.css'] = () => {}
+// require.extensions['.scss'] = () => {}
+// require.extensions['.css'] = () => {}
 global.window = {}
+console.log('It\'s still under development. Do not try to use!')
 require('./server')
 // SSR is under development, be patient :)
