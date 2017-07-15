1d50a6f1b3eafa68c4494e48186925f467380745
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-07-16 00:31:00 +0300

    feat(src/server/server): server use env vars PORT and BASE_API

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,13 @@
 import app from './express'
 import API from './api'
 import useSSR from './ssr'
-
-const httpPort = process.env.PORT || 3000
-
-// add handler for non-static requests
-app.use('/api/v1', API)
-// SSR
+// see src/server/index.js
+const {PORT, BASE_API} = process.env
+// Add API route
+app.use(BASE_API, API)
+// Add SSR handler
 app.get('*', useSSR)
-
-app.listen(httpPort, () => {
+// Start server
+app.listen(PORT, () => {
   console.log(`SSR ON PORT ${httpPort}! SSR!`)
 })
