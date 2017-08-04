b5e3c82dc4b70b98db047e05be39f2fc661a96aa
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-04 14:00:25 +0300

    refactor(webpack_config/server): move server's webpack config to own folder

---
+++ webpack_config/server/webpack.base.js
@@ -0,0 +1,105 @@
+'use strict'
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const config = require('../config')
+const exec = require('child_process').execSync
+
+exec(`rm -rf ${config.distPath}/server`)
+
+const {
+  BASE_API,
+  SENTRY_ID,
+  NODE_ENV,
+  DIST_PATH,
+  APP_LANGUAGE,
+  JWT_SECRET,
+  PORT
+} = process.env
+
+const language = APP_LANGUAGE || 'en'
+
+const definePluginArgs = {
+  'process.env.BROWSER': JSON.stringify(false),
+  'process.env.PORT': JSON.stringify(PORT || 4000),
+  'process.env.JWT_SECRET': JSON.stringify(JWT_SECRET || 'secret'),
+  'process.env.APP_LANGUAGE': JSON.stringify(language),
+  'process.env.NODE_ENV': JSON.stringify(NODE_ENV || 'development'),
+  'process.env.BASE_API': JSON.stringify(BASE_API || '/api/v1'),
+  'process.env.SENTRY_ID': JSON.stringify(SENTRY_ID),
+  'process.env.DIST_PATH': JSON.stringify(
+    DIST_PATH || path.join(config.distPath, './client', language)
+  )
+}
+
+let nodeModules = {}
+fs
+  .readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod
+  })
+
+module.exports = {
+  entry: [path.join(config.srcPath, './server/index')],
+  target: 'node',
+  output: {
+    path: path.join(config.distPath, './server'),
+    filename: 'index.js'
+  },
+  externals: nodeModules,
+  performance: {
+    hints: false
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: config.alias,
+    modules: [
+      // places where to search for required modules
+      config.srcPath,
+      path.join(config.rootPath, './node_modules')
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true
+            }
+          }
+        ],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: [/node_modules/]
+      }
+    ]
+  },
+  externals: nodeModules,
+  plugins: [
+    new webpack.DefinePlugin(definePluginArgs),
+    new webpack.BannerPlugin({
+      banner: config.banner
+    }),
+    // NOTE: Server can't process your styles/images/fonts on server-side rendering
+    new webpack.NormalModuleReplacementPlugin(
+      /\.(css|sass|less|jpg|png|gif|scss)$/,
+      'node-noop'
+    )
+    // NOTE: @Metnew: If I remember correctly this plugin works similarly to NormalModuleReplacementPlugin
+    // new webpack.IgnorePlugin(/\.(css|sass|less|jpg|png|gif|scss)$/)
+  ],
+  node: {
+    __dirname: true,
+    global: true
+  }
+}
---
+++ webpack_config/server/webpack.prod.js
@@ -0,0 +1,37 @@
+'use strict'
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const _ = require('lodash')
+const baseWebpackConfig = require('./webpack.base')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const ShakePlugin = require('webpack-common-shake').Plugin
+
+const plugins = [
+  new webpack.ProgressPlugin(),
+  new webpack.optimize.ModuleConcatenationPlugin(),
+  new ShakePlugin(),
+  // NOTE: you can use BabiliPlugin as an alternative to UglifyJSPlugin
+  // new BabiliPlugin(),
+  new UglifyJSPlugin({
+    sourceMap: true,
+    compress: {
+      warnings: false,
+      // This option removes console log in production
+      drop_console: true
+    },
+    output: {
+      comments: false
+    }
+  }),
+  new webpack.NormalModuleReplacementPlugin(
+    /\.(css|sass|less|jpg|png|gif|scss)$/,
+    'node-noop'
+  )
+  // new webpack.IgnorePlugin(/\.(css|sass|less|jpg|png|gif|scss)$/)
+]
+
+module.exports = Object.assign({}, baseWebpackConfig, {
+  devtool: 'cheap-source-map',
+  plugins: baseWebpackConfig.plugins.concat(plugins)
+})
