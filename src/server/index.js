8a360a2ff2a15017eca52b721dd234033b5aef65
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-04 13:56:39 +0300

    feat(src/server): remove env vars defaults, remove global.window patch, remove babel-core/register

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,5 @@
-require('babel-core/register')
-// NOTE: uncomment these lines below if you use scss/sass!
-// require.extensions['.scss'] = () => {}
-// require.extensions['.css'] = () => {}
-global.window = {
-	addEventListener: () => {}
-}
-const {BASE_API, PORT} = process.env
-process.env.BASE_API = BASE_API ? BASE_API : '/api/v1'
-process.env.PORT = PORT ? PORT : 4000
-global.i18n = () => {}
-global.fetch = require('node-fetch')
+global.i18n = str => str
+global.fetch = require('isomorphic-fetch')
+
+// import app
 require('./server')
