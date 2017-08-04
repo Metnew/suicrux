c22ce1c477b77fc7325c6c1dfdc8ffc76d708428
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-04 14:03:26 +0300

    refactor(webpack_config/config): add auth middleware, style fixes, get path for serveStatic from DIST_PATH env var

--- src/server/express/index.js
+++ src/server/express/index.js
@@ -1,43 +1,51 @@
 // Express-related stuff
 import express from 'express'
-import expressHelmet from 'helmet'
+import helmet from 'helmet'
 import compression from 'compression'
-import path from 'path'
-import fs from 'fs'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
+import jwt from 'jsonwebtoken'
+import chalk from 'chalk'
 // Application-related stuff
-// Do you remember that we use webpack aliases provided by cool babel plugin in .babelrc
 import {JWT_TOKEN} from 'common/api'
-//
-const language = process.env.APP_LANGUAGE || 'en'
-const distPath = `../../../dist/${language}`
+
 const app = express()
 // add express stuff
+app.use(helmet())
 app.use(compression())
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(
-  express.static(path.join(__dirname, distPath), {
+  express.static(process.env.DIST_PATH, {
     // don't use index.html inside /dist dir
     index: false
   })
 )
 app.disable('x-powered-by')
-app.use(expressHelmet())
 
 // Auth-related middleware, check that user is logged in and token is valid
 app.use((req, res, next) => {
-  //  Of course, you can make auth with jwt or cookies or session only
-  //  It's a starter project and I implemented a hardcoded solution
+  req.user = {}
   const token = req.cookies[JWT_TOKEN]
-  const isLoggedIn = token === 'nothing'
-  console.log(`USER IS LOGGED IN: ${isLoggedIn ? 'YES': 'NO'}`)
-  req.user = {
-    token,
-    isLoggedIn
+  if (!token) {
+    return next()
   }
-  next()
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(chalk.red('CANT DECODE JWT TOKEN!', err))
+    } else {
+      req.user = {
+        ...decoded,
+        token,
+        isLoggedIn: true
+      }
+    }
+    console.log(
+      chalk.blue(`USER IS LOGGED IN: ${req.user.isLoggedIn ? 'YES' : 'NO'}`)
+    )
+    next()
+  })
 })
 
 export default app
