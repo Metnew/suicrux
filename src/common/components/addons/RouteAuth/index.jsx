75e53ae8d59a79c18c96687f8d4fdd97db7c7bcb
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-05 18:42:59 +0300

    feat(src/common/components/addons/RouteAuth): remove handling on lazy loading from RouteAuth

---
+++ src/common/components/addons/RouteAuth/index.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {Route, Redirect} from 'react-router-dom'
+
+/**
+ * Component that protects route from unauthorized users.
+ */
+class RouteAuth extends Component {
+ static propTypes = {
+   canAccess: PropTypes.func,
+   path: PropTypes.string
+ }
+
+ render () {
+   const {canAccess, path} = this.props
+
+   console.log(
+     `User has access to "${path}" path: ${canAccess(path) ? 'YES' : 'NO'}`
+   )
+
+   return canAccess(path) ? <Route {...this.props} /> : <Redirect to="/auth" />
+ }
+}
+
+export default RouteAuth
