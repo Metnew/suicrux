78a4b29a33969f87e0444fa18adfcbddf769e1a7
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-05 19:28:28 +0300

    feat(src/client/index): allow to run whyDidYouUpdate

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -12,9 +12,22 @@ if (process.env.NODE_ENV === 'production') {
   require('common/pwa')
 } else if (process.env.NODE_ENV === 'development') {
   // Devtools
-  // whyDidYouUpdate package is temporary broken, waiting for a patch
-  // const {whyDidYouUpdate} = require('why-did-you-update')
-  // whyDidYouUpdate(React)
+  // NOTE: whyDidYouUpdate package is temporary broken, waiting for a patch.
+
+  /*eslint-disable */
+	// XXX: But if you really want to run `why-did-you-update`
+	// You can uncomment this block:
+	/*
+    Object.defineProperty(React, 'createClass', {
+      set: nextCreateClass => {
+        createClass = nextCreateClass
+      }
+    })
+
+   const {whyDidYouUpdate} = require('why-did-you-update')
+    whyDidYouUpdate(React)
+  */
+	/*eslint-enable */
   window.Perf = require('react-addons-perf')
 }
 
