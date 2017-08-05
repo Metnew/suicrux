ab475f879ed9c706a615fe80e63a73cfe50f44df
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-05 12:10:10 +0300

    docs(src/common/containers): add comment

--- src/common/containers/index.js
+++ src/common/containers/index.js
@@ -2,3 +2,5 @@ export App from './App'
 export Dashboard from './Dashboard'
 export Users from './Users'
 export Login from './Login'
+// NOTE: Export only containers that aren't lazy-loaded!
+// Else lazy containers would be imported in the main bundle!
