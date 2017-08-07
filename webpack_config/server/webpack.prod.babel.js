098aec278151a5a2e8b8ad3c46af81e1b9e5c616
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-07 18:40:01 +0300

    refactor(): remove duplication of NormalModuleReplacement and IgnorePlugin

--- webpack_config/server/webpack.prod.babel.js
+++ webpack_config/server/webpack.prod.babel.js
@@ -5,30 +5,25 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const ShakePlugin = require('webpack-common-shake').Plugin
 
 const plugins = [
-  new webpack.ProgressPlugin(),
-  new webpack.optimize.ModuleConcatenationPlugin(),
-  new ShakePlugin(),
-  // NOTE: you can use BabiliPlugin as an alternative to UglifyJSPlugin
-  // new BabiliPlugin(),
-  new UglifyJSPlugin({
-    sourceMap: true,
-    compress: {
-      warnings: false
-      // This option removes console log in production
-      // drop_console: true
-    },
-    output: {
-      comments: false
-    }
-  }),
-  new webpack.NormalModuleReplacementPlugin(
-    /\.(css|sass|less|jpg|png|gif|scss)$/,
-    'node-noop'
-  )
-  // new webpack.IgnorePlugin(/\.(css|sass|less|jpg|png|gif|scss)$/)
+	new webpack.ProgressPlugin(),
+	new webpack.optimize.ModuleConcatenationPlugin(),
+	new ShakePlugin(),
+	// NOTE: you can use BabiliPlugin as an alternative to UglifyJSPlugin
+	// new BabiliPlugin(),
+	new UglifyJSPlugin({
+		sourceMap: true,
+		compress: {
+			warnings: false
+			// This option removes console log in production
+			// drop_console: true
+		},
+		output: {
+			comments: false
+		}
+	})
 ]
 
 module.exports = Object.assign({}, baseWebpackConfig, {
-  devtool: 'cheap-source-map',
-  plugins: baseWebpackConfig.plugins.concat(plugins)
+	devtool: 'cheap-source-map',
+	plugins: baseWebpackConfig.plugins.concat(plugins)
 })
