e9d4bd8834ed57ca939d0528bfc3cd5ada53947a
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-09 19:22:28 +0300

    refactor(webpack_config/config): assign process.env with fallbacks to config.js, rewrite in es6

--- webpack_config/config.js
+++ webpack_config/config.js
@@ -2,96 +2,60 @@
  * @file for config stuff that's used for webpack configuration, but isn't passed to webpack compiler
  */
 
-const path = require('path')
+import path from 'path'
+import i18n from '../i18n'
+import manifest from './assets/manifest'
+
+function ternary (a, b) {
+	return a ? a : b
+}
+
+// Vars for both server and frontend
+const BASE_API = ternary(process.env.BASE_API, '/api/v1')
+const APP_LANGUAGE = ternary(process.env.APP_LANGUAGE, 'en')
+const NODE_ENV = ternary(process.env.NODE_ENV, 'development')
+
+// Vars for frontend only
+const SENTRY_DSN_PUBLIC = ternary(process.env.SENTRY_DSN_PUBLIC)
+const GA_ID = ternary(process.env.GA_ID)
+
+// Paths
 const rootPath = path.join(__dirname, '../') // = "/"
 const distPath = path.join(rootPath, './dist') // = "/dist"
 const srcPath = path.join(rootPath, './src') // = "/src"
 const srcCommonPath = path.join(srcPath, './common') // = "/src/common"
-const i18n = require('../i18n')
-module.exports = {
-	port: process.env.PORT || 3000,
+
+// Vars for server only
+const DIST_PATH = ternary(process.env.DIST_PATH, path.join(distPath, './client', APP_LANGUAGE))
+const JWT_SECRET = ternary(process.env.JWT_SECRET, 'secret')
+const SENTRY_DSN = ternary(process.env.SENTRY_DSN)
+const PORT = ternary(process.env.PORT, 4000)
+
+
+export default {
 	title: 'React-Semantic.UI-Starter',
 	publicPath: '/',
+	// i18n object
 	i18n,
+	// Env vars
+	BASE_API,
+	APP_LANGUAGE,
+	NODE_ENV,
+	SENTRY_DSN_PUBLIC,
+	GA_ID,
+	DIST_PATH,
+	JWT_SECRET,
+	SENTRY_DSN,
+	PORT,
 	// It's better to define pathes in one file
 	// and then use everywhere across app
 	srcPath,
 	srcCommonPath,
 	distPath,
 	rootPath,
-	// base places where server and client both search for modules
-	modules: [],
-	// aliases that both server and client use
-	alias: {
-		actions: `${srcCommonPath}/actions/`,
-		api: `${srcCommonPath}/api/`,
-		components: `${srcCommonPath}/components/`,
-		containers: `${srcCommonPath}/containers/`,
-		reducers: `${srcCommonPath}/reducers/`,
-		routing: `${srcCommonPath}/routing/`,
-		styles: `${srcCommonPath}/styles/`,
-		static: `${rootPath}/static`,
-		images: `${rootPath}/static/images`
-	},
-	// text for WebpackBannerPlugin,
-	// this plugin just adds text banner to the beginning of your file
+	// text for WebpackBannerPlugin
 	banner:
 		'MIT License. Copyright (c) 2017 Vladimir Metnew All Rights Reserved. Repo: https://github.com/Metnew/react-semantic.ui-starter',
 	// your manifest.json
-	manifest: {
-		name: 'React-Semantic.UI-Starter',
-		short_name: 'RSUIS',
-		description: 'https://github.com/Metnew/react-semantic.ui-starter',
-		icons: [
-			{
-				src: 'favicons/android-chrome-36x36.png',
-				sizes: '36x36',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-48x48.png',
-				sizes: '48x48',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-72x72.png',
-				sizes: '72x72',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-96x96.png',
-				sizes: '96x96',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-144x144.png',
-				sizes: '144x144',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-192x192.png',
-				sizes: '192x192',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-256x256.png',
-				sizes: '256x256',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-384x384.png',
-				sizes: '384x384',
-				type: 'image/png'
-			},
-			{
-				src: 'favicons/android-chrome-512x512.png',
-				sizes: '512x512',
-				type: 'image/png'
-			}
-		],
-		start_url: '.',
-		display: 'standalone',
-		background_color: '#f7f7f7',
-		theme_color: '#009688'
-	}
+	manifest
 }
