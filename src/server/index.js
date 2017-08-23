039df01852530b9bec7808b4fe83039fab7c7e5a
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-08-23 19:21:38 +0300

    feat(server): add server entry file
    feat(server): add server entry file

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,21 @@
-global.i18n = str => str
-global.fetch = require('node-fetch')
+/**
+ * @flow
+ * @file
+ */
+import express from 'express'
+import chalk from 'chalk'
+// import serverDecorator from './server'
+const app: any = express()
+const PORT: number = process.env.PORT || 3000
+const serverModulePath =
+	process.env.NODE_ENV === 'production'
+		? './server' // Decorate server with all middlewares/routes for production
+		: '../../webpack_config/server' // Mount our server-side code to dev server
 
-// Import app
-require('./server')
+const createServer: Function = require(serverModulePath).default
+createServer(app)
+
+// Start server
+app.listen(PORT, () => {
+	console.log(chalk.green(`SERVER IS LISTENING ON ${PORT}`))
+})
