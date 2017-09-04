234c8174fb545ffc8ff314ae7a0e410d5715b348
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-04 15:53:14 +0300

    fix(app/configure_root): add flow, add fix for hmr for reducer
    fix(app/configure_root): add flow, add fix for hmr for reducer

--- src/common/app/configure_root.jsx
+++ src/common/app/configure_root.jsx
@@ -10,17 +10,29 @@ import Root from 'components/parts/Root'
 import 'styles/global'
 
 type Props = {
-	store: Object,
+	store: Object, // unconfigured store
 	SSR: {
+		// SSR options, see `/server` for more info
 		context: Object,
 		location: string
 	},
-	history: Object,
-	routes: mixed // RouteItem[]
+	history: Object, // Root node with routing, synced history, store
+	routes: RouteItem[] // app routing
 }
+
+/**
+ * Configure Root component.
+ */
 
 const configureRootComponent = ({store, SSR, history, routes}: Props) => {
 	const props = {store, SSR, history, routes}
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('reducers', () => {
+			const nextRootReducer = require('reducers').default
+			store.replaceReducer(nextRootReducer)
+		})
+	}
 	return <Root {...props} />
 }
 
