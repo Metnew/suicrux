b6a1a5994e00205e99bfd3cc5e54455aeeabbb90
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-04 16:12:00 +0300

    fix(server/index): add more flow types
    fix(server/index): add more flow types

--- src/server/index.js
+++ src/server/index.js
@@ -4,10 +4,9 @@
  */
 import express from 'express'
 import chalk from 'chalk'
-// import serverDecorator from './server'
-const app: any = express()
-const PORT: number = process.env.PORT || 3000
-const serverModulePath =
+const app: express$Application = express()
+const PORT: number = +process.env.PORT || 3000
+const serverModulePath: string =
 	process.env.NODE_ENV === 'production'
 		? './server' // Decorate server with all middlewares/routes for production
 		: '../../webpack_config/server' // Mount our server-side code to dev server
