54a5b4c7a25601dcde5c3aee767235f046a2c6c0
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-04 16:05:38 +0300

    feat(reducers): rewrite reducers using flow, fix object properties
    feat(reducers): rewrite reducers using flow:
    - typed actions
    - add types of reducer’s state
    
    fix object properties: action.result -> actions.payload

--- src/common/reducers/posts/index.js
+++ src/common/reducers/posts/index.js
@@ -1,21 +1,40 @@
+// @flow
+import {LOCATION_CHANGE} from 'actions/common'
 import {
-	LOCATION_CHANGE,
 	GET_POSTS_SUCCESS,
 	GET_POSTS_PENDING,
 	GET_POSTS_FAIL
-} from 'actions'
+} from 'actions/posts'
 import {normalizeArrayOfItems} from 'api/utils'
+import type {
+	GET_POSTS_FAIL_TYPE,
+	GET_POSTS_SUCCESS_TYPE,
+	GET_POSTS_PENDING_TYPE
+} from 'actions/posts'
 
-export const initialState = {
+export type State = {
+	entities: Object,
+	errors: Object,
+	isLoading: boolean,
+	isLoaded: boolean,
+	count: number
+}
+
+type Action =
+	| GET_POSTS_FAIL_TYPE
+	| GET_POSTS_SUCCESS_TYPE
+	| GET_POSTS_PENDING_TYPE
+	| GET_POSTS_FAIL_TYPE
+
+export const initialState: State = {
 	entities: {},
 	errors: {},
-	fetchStatus: 'none',
 	isLoading: false,
 	isLoaded: false,
 	count: 0
 }
 
-export function posts (state = initialState, action) {
+export function posts (state: State = initialState, action: Action): State {
 	switch (action.type) {
 	case LOCATION_CHANGE: {
 		const {pathname} = action.payload
@@ -29,31 +48,29 @@ export function posts (state = initialState, action) {
 			...state,
 			errors: {},
 			isLoaded: false,
-			isLoading: true,
-			fetchStatus: 'loading'
+			isLoading: true
 		}
 	}
 	case GET_POSTS_SUCCESS:
-		const {result} = action
+		const {payload} = action
 		// @Metnew:
 		// result may be an object, if it was request with params
 		// `normalizeArrayOfItems` normalize only arrays of items!
-		const {count, entities} = normalizeArrayOfItems([result])
+		const {count, entities} = normalizeArrayOfItems([payload])
 		return {
 			isLoaded: true,
 			isLoading: false,
-			fetchStatus: 'loaded',
 			errors: {},
 			count,
 			entities
 		}
 	case GET_POSTS_FAIL:
+		const {errors} = action.payload
 		return {
 			...state,
 			isLoaded: true,
 			isLoading: false,
-			fetchStatus: 'loaded',
-			errors: action.errors
+			errors
 		}
 	default:
 		return state
--- src/common/reducers/users/index.js
+++ src/common/reducers/users/index.js
@@ -1,21 +1,37 @@
+// @flow
+import {LOCATION_CHANGE} from 'actions/common'
 import {
-	LOCATION_CHANGE,
 	GET_USERS_SUCCESS,
 	GET_USERS_FAIL,
 	GET_USERS_PENDING
-} from 'actions'
+} from 'actions/users'
 import {normalizeArrayOfItems} from 'api/utils'
+import type {LOCATION_CHANGE_TYPE} from 'actions/common'
+import type {GET_USERS_SUCCESS_TYPE, GET_USERS_FAIL_TYPE, GET_USERS_PENDING_TYPE} from 'actions/users'
 
-export const initialState = {
+export type State = {
+	entities: Object,
+	errors: Object,
+	isLoading: boolean,
+	isLoaded: boolean,
+	count: number
+}
+
+type Action =
+	| LOCATION_CHANGE_TYPE
+	| GET_USERS_SUCCESS_TYPE
+	| GET_USERS_FAIL_TYPE
+	| GET_USERS_PENDING_TYPE
+
+export const initialState: State = {
 	entities: {},
 	errors: {},
-	fetchStatus: 'none',
 	isLoading: false,
 	isLoaded: false,
 	count: 0
 }
 
-export function users (state = initialState, action) {
+export function users (state: State = initialState, action: Action): State {
 	switch (action.type) {
 	case LOCATION_CHANGE: {
 		// Return state if path matches /users
@@ -31,34 +47,32 @@ export function users (state = initialState, action) {
 			...state,
 			errors: {},
 			isLoaded: false,
-			isLoading: true,
-			fetchStatus: 'loading'
+			isLoading: true
 		}
 	}
 	case GET_USERS_SUCCESS: {
-		const {result} = action
 		// @Metnew:
 		// result may be an object, if it was request with params
-		// `normalizeArrayOfItems` normalize only arrays of items!
-		const {count, entities} = normalizeArrayOfItems([result])
-		// Normalizr here
+		// `normalizeArrayOfItems` works only with arrays!
+		const {count, entities} = normalizeArrayOfItems([action.payload])
+		console.log(entities)
 		return {
 			...state,
 			isLoaded: true,
 			isLoading: false,
-			fetchStatus: 'loaded',
 			entities,
 			count
 		}
 	}
-	case GET_USERS_FAIL:
+	case GET_USERS_FAIL: {
+		const {errors} = action.payload
 		return {
 			...state,
-			errors: action.errors,
+			errors,
 			isLoaded: true,
-			isLoading: false,
-			fetchStatus: 'loaded'
+			isLoading: false
 		}
+	}
 	default:
 		return state
 	}
