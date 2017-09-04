55b26992a35b070f5c9c8d00062c247c438cba12
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-04 16:06:13 +0300

    fix(routing): add more types, use absolute imports for containers
    fix(routing): add more types, use absolute imports for containers

--- src/common/routing/index.jsx
+++ src/common/routing/index.jsx
@@ -1,16 +1,20 @@
 /**
  * @flow
  */
-import * as React from 'react'
+import React from 'react'
 import {Route} from 'react-router-dom'
-import {Users, Dashboard, Login} from 'containers'
+import Dashboard from 'containers/Dashboard'
+import Login from 'containers/Login'
+import Users from 'containers/Users'
+
 import RouteAuth from 'components/addons/RouteAuth'
 import {createBrowserHistory, createMemoryHistory} from 'history'
+import type {RouteItem} from 'types'
 
 export const history = getHistory()
 
-const loadLazyComponent = url => {
-	return async cb => {
+const loadLazyComponent = (url: string) => {
+	return async (cb: Function) => {
 		// NOTE: there isn't any duplication here
 		// Read Webpack docs about code-splitting for more info.
 		if (process.env.BROWSER) {
@@ -22,18 +26,7 @@ const loadLazyComponent = url => {
 	}
 }
 
-export type RouteItem = {
-	path: string,
-	exact?: boolean,
-	strict?: boolean,
-	icon?: string,
-	name?: string,
-	sidebarVisible?: boolean,
-	tag?: React.Node,
-	component?: React.Node
-}
-
-export const routes = [
+export const routes: Array<RouteItem> = [
 	{
 		path: '/',
 		exact: true,
