bda6b45d477ccd497acd63c67ac1686c35380508
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-09 18:02:12 +0300

    feat(parts/Root): add IntlProvider
    feat(parts/Root): add IntlProvider

--- src/common/components/parts/Root/index.jsx
+++ src/common/components/parts/Root/index.jsx
@@ -1,7 +1,7 @@
 // @flow
 import React, {Component} from 'react'
 import {Provider} from 'react-redux'
-// import {I18nextProvider} from 'react-i18next' // as we build ourself via webpack
+import {IntlProvider} from 'react-intl' // as we build ourself via webpack
 import {APPLICATION_INIT} from 'actions/common'
 import {ThemeProvider} from 'styled-components'
 import theme from 'styles/theme'
@@ -39,23 +39,23 @@ export default class Root extends Component {
 
 	render () {
 		const {SSR, store, history, routes} = this.props
-		const routerProps =
-			process.env.BROWSER === true
-				? {history}
-				: {location: SSR.location, context: SSR.context}
+		const routerProps = process.env.BROWSER
+			? {history}
+			: {location: SSR.location, context: SSR.context}
 		// key={Math.random()} = hack for HMR from https://github.com/webpack/webpack-dev-server/issues/395
+
 		return (
-			// <I18nextProvider i18n={{}}>
-			<Provider store={store} key={Math.random()}>
-				<ThemeProvider theme={theme}>
-					<Router {...routerProps} key={Math.random()}>
-						<App routes={routes}>
-							<RoutingWrapper store={store} routes={routes} />
-						</App>
-					</Router>
-				</ThemeProvider>
-			</Provider>
-			// </I18nextProvider>
+			<IntlProvider locale="en-US" messages={{}}>
+				<Provider store={store} key={Math.random()}>
+					<ThemeProvider theme={theme}>
+						<Router {...routerProps} key={Math.random()}>
+							<App routes={routes}>
+								<RoutingWrapper store={store} routes={routes} />
+							</App>
+						</Router>
+					</ThemeProvider>
+				</Provider>
+			</IntlProvider>
 		)
 	}
 }
