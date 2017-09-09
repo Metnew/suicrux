e1133881f36131410823b4e95ca2263a72ae1797
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-10 00:15:36 +0300

    refactor(Users): use func component instead of PureComponent
    refactor(Users): use func component instead of PureComponent

--- src/common/containers/Users/components/index.jsx
+++ src/common/containers/Users/components/index.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, {PureComponent} from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Item, Grid} from 'semantic-ui-react'
 import _ from 'lodash'
@@ -12,35 +12,24 @@ type Props = {
 	isUsersLoaded: boolean,
 	usersCount: number
 }
+// <FormattedMessage
+const UsersComponent = ({users, isUsersLoaded, usersCount}: Props) => {
+	// IsUsersLoading
+	const noUsers = isUsersLoaded && usersCount === 0
 
-class UsersComponent extends PureComponent {
-	props: Props
-
-	// ShouldComponentUpdate (nextProps) {
-	//   const {users} = this.props
-	//   const nextUsers = nextProps.users
-	//   return !_.isEqual(users, nextUsers)
-	// }
-
-	render () {
-		// IsUsersLoading
-		const {users, isUsersLoaded, usersCount} = this.props
-		const noUsers = isUsersLoaded && usersCount === 0
-
-		return (
-			<Grid stackable>
-				<Grid.Column width={16}>
-					{noUsers
-						? 'Hm, it looks like there are no items to show :('
-						: <Item.Group divided link>
-							{_.map(users, (user: UserItem, i) => {
-								return <UsersItemComponent key={i} user={user} />
-							})}
-						</Item.Group>}
-				</Grid.Column>
-			</Grid>
-		)
-	}
+	return (
+		<Grid stackable>
+			<Grid.Column width={16}>
+				{noUsers
+					? 'Hm, it looks like there are no items to show :('
+					: <Item.Group divided link>
+						{_.map(users, (user: UserItem, i) => {
+							return <UsersItemComponent key={i} user={user} />
+						})}
+					</Item.Group>}
+			</Grid.Column>
+		</Grid>
+	)
 }
 
 export default UsersComponent
