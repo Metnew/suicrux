8a923d3b9174accb61c1f51c52506e81cb533440
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-09 18:06:03 +0300

    feat(ssr): add translations for ssr
    feat(ssr): add translations for ssr

--- src/server/ssr/index.js
+++ src/server/ssr/index.js
@@ -2,17 +2,30 @@
  * @flow
  * @desc
  */
-import React, {type Node} from 'react'
-import fs from 'fs'
+import React from 'react'
+import {readFileSync} from 'fs'
+import path from 'path'
+import {sync as globSync} from 'glob'
 import {renderToString} from 'react-dom/server'
 import Helmet from 'react-helmet'
 import createHistory from 'history/createMemoryHistory'
 import {ServerStyleSheet, StyleSheetManager} from 'styled-components'
-import {configureApp, configureRootComponent} from 'common/app'
+import {configureRootComponent, configureApp} from 'common/app'
 import IndexHTMLComponent from './IndexHTMLComponent'
+import type {Node} from 'react'
 import assets from 'webpack-assets'
 import faviconsAssets from 'favicons-assets'
 
+const translations = globSync('locals/*.json')
+	.map(filename => [
+		path.basename(filename, '.json'),
+		readFileSync(filename, 'utf8')
+	])
+	.map(([locale, file]) => [locale, JSON.parse(file)])
+	.reduce((collection, [locale, messages]) => {
+		collection[locale] = messages
+		return collection
+	}, {})
 export default (req: Object, res: Object, next: () => void) => {
 	const {isLoggedIn, token} = req.user
 	const initialState: Object = isLoggedIn
@@ -28,12 +41,12 @@ export default (req: Object, res: Object, next: () => void) => {
 		routes,
 		SSR: {location, context}
 	})
-	const App: Node = (
+	const App: string = renderToString(
 		<StyleSheetManager sheet={sheet.instance}>
 			{RootComponent}
 		</StyleSheetManager>
 	)
-	const css = sheet.getStyleTags()
+	const css: string = sheet.getStyleTags()
 	const preloadedState: Object = store.getState()
 	const props = {
 		css,
@@ -42,6 +55,6 @@ export default (req: Object, res: Object, next: () => void) => {
 		initialState: preloadedState,
 		App
 	}
-	const html: string = renderToString(<IndexHTMLComponent {...props} />)
+	const html: string = IndexHTMLComponent(props)
 	res.send(html)
 }
