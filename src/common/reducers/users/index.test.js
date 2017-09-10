64a3a51931db83ba7036cfd523f76d141ababae1
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-10 17:24:10 +0300

    fix(reducers/users): fix tests for users reducer
    fix(reducers/users): fix tests for users reducer

--- src/common/reducers/users/index.test.js
+++ src/common/reducers/users/index.test.js
@@ -1,29 +1,61 @@
 import {users as reducer, initialState} from 'reducers/users'
-import * as actions from 'actions'
+import {
+	GET_USERS_FAIL,
+	GET_USERS_SUCCESS,
+	GET_USERS_PENDING
+} from 'actions/users'
+import {LOCATION_CHANGE} from 'actions/common'
 
-const GET_USERS_FAIL = {
-	type: actions.GET_USERS_FAIL,
-	errors: {
-		hmm: 'thatsanerror'
+const fail = {
+	type: GET_USERS_FAIL,
+	payload: {
+		errors: {
+			hmm: 'thatsanerror'
+		}
 	}
 }
-const GET_USERS_SUCCESS = {
-	type: actions.GET_USERS_SUCCESS,
-	result: [{id: 1, something: 'strange'}]
-}
-const GET_USERS_PENDING = {
-	type: actions.GET_USERS_PENDING
+
+const sampleUserItem = {
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz',
+	address: {
+		street: 'Kulas Light',
+		suite: 'Apt. 556',
+		city: 'Gwenborough',
+		zipcode: '92998-3874',
+		geo: {
+			lat: '-37.3159',
+			lng: '81.1496'
+		}
+	},
+	phone: '1-770-736-8031 x56442',
+	website: 'hildegard.org',
+	company: {
+		name: 'Romaguera-Crona',
+		catchPhrase: 'Multi-layered client-server neural-net',
+		bs: 'harness real-time e-markets'
+	}
 }
 
-const LOCATION_CHANGE_INBOX_ID = {
-	type: actions.LOCATION_CHANGE,
+const success = {
+	type: GET_USERS_SUCCESS,
+	payload: sampleUserItem
+}
+const pending = {
+	type: GET_USERS_PENDING
+}
+
+const locationChangeToInboxId = {
+	type: LOCATION_CHANGE,
 	payload: {
 		pathname: '/users/1'
 	}
 }
 
-const LOCATION_CHANGE_TO_OTHER_PATH = {
-	type: actions.LOCATION_CHANGE,
+const locationChangeDashboard = {
+	type: LOCATION_CHANGE,
 	payload: {
 		pathname: '/'
 	}
@@ -35,40 +67,34 @@ describe('USERS REDUCER', () => {
 	})
 
 	it('should handle GET_USERS_SUCCESS', () => {
-		expect(reducer(initialState, GET_USERS_SUCCESS)).toEqual({
+		expect(reducer(initialState, success)).toEqual({
 			...initialState,
 			entities: {
-				'1': {
-					id: 1,
-					something: 'strange'
-				}
+				'1': sampleUserItem
 			},
 			count: 1,
 			isLoaded: true,
-			isLoading: false,
-			fetchStatus: 'loaded'
+			isLoading: false
 		})
 	})
 
 	it('should handle GET_USERS_FAIL', () => {
-		expect(reducer(initialState, GET_USERS_FAIL)).toEqual({
+		expect(reducer(initialState, fail)).toEqual({
 			...initialState,
 			errors: {
 				hmm: 'thatsanerror'
 			},
 			isLoaded: true,
-			isLoading: false,
-			fetchStatus: 'loaded'
+			isLoading: false
 		})
 	})
 
 	it('should handle GET_USERS_PENDING', () => {
-		expect(reducer(initialState, GET_USERS_PENDING)).toEqual({
+		expect(reducer(initialState, pending)).toEqual({
 			...initialState,
 			errors: {},
 			isLoaded: false,
-			isLoading: true,
-			fetchStatus: 'loading'
+			isLoading: true
 		})
 	})
 
@@ -77,9 +103,7 @@ describe('USERS REDUCER', () => {
 			...initialState,
 			hello: 'world'
 		}
-		expect(reducer(customState, LOCATION_CHANGE_INBOX_ID)).toEqual(
-			customState
-		)
+		expect(reducer(customState, locationChangeToInboxId)).toEqual(customState)
 	})
 
 	it('should handle LOCATION_CHANGE to not /users path', () => {
@@ -87,6 +111,6 @@ describe('USERS REDUCER', () => {
 			...initialState,
 			hello: 'world'
 		}
-		expect(reducer(customState, LOCATION_CHANGE_TO_OTHER_PATH)).toEqual(initialState)
+		expect(reducer(customState, locationChangeDashboard)).toEqual(initialState)
 	})
 })
