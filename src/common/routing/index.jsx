a1231ecfcb8fbba97a593535d55a9d1bc92ecf48
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-10 14:34:54 +0300

    feat(routing): make all containers lazy, simplify code
    feat(routing): make all containers lazy, simplify code

--- src/common/routing/index.jsx
+++ src/common/routing/index.jsx
@@ -3,47 +3,50 @@
  */
 import React from 'react'
 import {Route} from 'react-router-dom'
-import Dashboard from 'containers/Dashboard'
-import Login from 'containers/Login'
-import Users from 'containers/Users'
-
 import RouteAuth from 'components/addons/RouteAuth'
 import {createBrowserHistory, createMemoryHistory} from 'history'
 import type {RouteItem} from 'types'
 
+const getHistory = () => {
+	if (process.env.BROWSER) {
+		return createMemoryHistory()
+	}
+	return createBrowserHistory()
+}
+
 export const history = getHistory()
 
-const loadLazyComponent = (url: string) => {
-	return async (cb: Function) => {
-		// NOTE: there isn't any duplication here
-		// Read Webpack docs about code-splitting for more info.
-		if (process.env.BROWSER) {
-			const loadComponent = await import(/* webpackMode: "lazy-once", webpackChunkName: "lazy-containers" */ `containers/${url}/index.jsx`)
-			return loadComponent
-		}
-		const loadComponent = await import(/* webpackMode: "eager", webpackChunkName: "lazy-containers" */ `containers/${url}/index.jsx`)
+const loadLazyComponent = (url: string) => async () => {
+	// NOTE: there isn't any duplication here
+	// Read Webpack docs about code-splitting for more info.
+	if (process.env.BROWSER) {
+		const loadComponent = await import(/* webpackMode: "lazy-once", webpackChunkName: "lazy-containers" */ `containers/${url}/index.jsx`)
 		return loadComponent
 	}
+	const loadComponent = await import(/* webpackMode: "eager", webpackChunkName: "lazy-containers" */ `containers/${url}/index.jsx`)
+	return loadComponent
 }
 
 export const routes: Array<RouteItem> = [
 	{
 		path: '/',
 		exact: true,
+		lazy: true,
 		icon: 'newspaper',
 		name: 'Dashboard',
 		sidebarVisible: true,
 		tag: RouteAuth,
-		component: Dashboard
+		component: loadLazyComponent('Dashboard')
 	},
 	{
 		path: '/users',
 		name: 'Users',
 		exact: true,
+		lazy: true,
 		icon: 'users',
 		sidebarVisible: true,
 		tag: RouteAuth,
-		component: Users
+		component: loadLazyComponent('Users')
 	},
 	{
 		external: true,
@@ -55,8 +58,9 @@ export const routes: Array<RouteItem> = [
 	{
 		path: '/auth',
 		name: 'Auth',
+		lazy: true,
 		tag: Route,
-		component: Login
+		component: loadLazyComponent('Login')
 	},
 	{
 		path: '/users/:id',
@@ -67,11 +71,3 @@ export const routes: Array<RouteItem> = [
 		component: loadLazyComponent('UserItem')
 	}
 ]
-
-function getHistory () {
-	const basename = ''
-	if (process.env.BROWSER !== true) {
-		return createMemoryHistory()
-	}
-	return createBrowserHistory({basename})
-}
