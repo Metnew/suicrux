249f4e2d294637873c9900fb8bb1804f133d5224
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-10 21:15:08 +0300

    fix(actions/posts): fix tests
    fix(actions/posts): fix tests

--- src/common/actions/posts/index.test.js
+++ src/common/actions/posts/index.test.js
@@ -1,20 +1,53 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import * as actions from 'actions'
+import {
+	GET_POSTS_SUCCESS,
+	GET_POSTS_PENDING,
+	GET_POSTS_FAIL,
+	GET_POSTS
+} from 'actions/posts'
+import {getPostsAPI} from 'api/PostsSvc'
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 describe('POSTS actions', () => {
-	it('creates GET_POSTS_SUCCESS when GET_POSTS was successful', done => {
+	it('creates GET_POSTS_SUCCESS when GET_POSTS was successful', async done => {
 		const store = mockStore({})
-		return store.dispatch(actions.GET_POSTS).then(res => {
-			const {result} = res
-			const expectedAction = {
-				type: actions.GET_POSTS_SUCCESS,
-				result
-			}
+		const apiResult = await getPostsAPI(1)
+		const samplePayload = apiResult.data
 
-			expect(res).toEqual(expectedAction)
+		store.dispatch(GET_POSTS(1)).then(() => {
+			const actions = store.getActions()
+			const success = {
+				type: GET_POSTS_SUCCESS,
+				payload: samplePayload
+			}
+			const pending = {
+				type: GET_POSTS_PENDING
+			}
+			const expectedActions = [pending, success]
+
+			expect(actions).toMatchObject(expectedActions)
+			done()
+		})
+	})
+
+	xit('creates GET_POSTS_FAIL when GET_POSTS was unsuccessful', done => {
+		const store = mockStore({})
+		return store.dispatch(GET_POSTS(1)).then(() => {
+			const actions = store.getActions()
+			const success = {
+				type: GET_POSTS_SUCCESS,
+				payload: []
+			}
+			const pending = {
+				type: GET_POSTS_PENDING
+			}
+			const expectedActions = [pending, success]
+
+			console.log(actions)
+
+			expect(actions).toEqual(expectedActions)
 			done()
 		})
 	})
