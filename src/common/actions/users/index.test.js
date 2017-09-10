91aa905e342cbddae152a5eccd9e31ebd8f56bba
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-10 21:16:23 +0300

    fix(actions/users): fix tests
    fix(actions/users): fix tests

--- src/common/actions/users/index.test.js
+++ src/common/actions/users/index.test.js
@@ -1,21 +1,49 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import * as actions from 'actions'
+import {
+	GET_USERS,
+	GET_USERS_SUCCESS,
+	GET_USERS_PENDING,
+	GET_USERS_FAIL
+} from 'actions/users'
+import {getUsersAPI} from 'api/UsersSvc'
+
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
-describe('USERS actions', () => {
-	it('creates GET_USERS_SUCCESS when GET_USERS was successful', done => {
-		const store = mockStore({})
-		// bind(null, null) - because we need somehow put args to a function
-		return store.dispatch(actions.GET_USERS.bind(null, null)).then(res => {
-			const {result} = res
-			const expectedAction = {
-				type: actions.GET_USERS_SUCCESS,
-				result
-			}
+const pending = {
+	type: GET_USERS_PENDING
+}
 
-			expect(res).toEqual(expectedAction)
+describe('USERS actions', () => {
+	it('creates GET_USERS_SUCCESS when GET_USERS was successful', async done => {
+		const store = mockStore({})
+		const apiResult = await getUsersAPI({id: 1})
+		const samplePayload = apiResult.data
+		return store.dispatch(GET_USERS(1)).then(res => {
+			const actions = store.getActions()
+			const success = {
+				type: GET_USERS_SUCCESS,
+				payload: samplePayload
+			}
+			const expectedActions = [pending, success]
+
+			expect(actions).toMatchObject(expectedActions)
+			done()
+		})
+	})
+
+	xit('creates GET_USERS_FAIL when GET_USERS was unsuccessful', done => {
+		const store = mockStore({})
+		return store.dispatch(GET_USERS(1)).then(res => {
+			const actions = store.getActions()
+			const fail = {
+				type: GET_USERS_FAIL,
+				payload: {}
+			}
+			const expectedActions = [pending, fail]
+
+			expect(actions).toEqual(expectedActions)
 			done()
 		})
 	})
