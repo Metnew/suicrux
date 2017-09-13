e9de3d6da5e3cfc03b6d5e39fe92ede9c110d712
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-14 00:39:25 +0300

    Revert "style(webpack_config/server): rewrite prod conf in functional style"
    This reverts commit bc707a71f85e918a2d95acd7ce1f15beb191525b.

--- webpack_config/server/webpack.prod.babel.js
+++ webpack_config/server/webpack.prod.babel.js
@@ -5,9 +5,6 @@ import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 import {Plugin as ShakePlugin} from 'webpack-common-shake'
 import OptimizeJsPlugin from 'optimize-js-plugin'
 
-const analyzePlugins = process.env.ANALYZE_BUNDLE
-	? [new BundleAnalyzerPlugin({analyzerMode: 'static'})]
-	: []
 const plugins = [
 	new webpack.ProgressPlugin(),
 	new webpack.optimize.ModuleConcatenationPlugin(),
@@ -29,9 +26,13 @@ const plugins = [
 	}),
 	new OptimizeJsPlugin({
 		sourceMap: true
-	}),
-	...analyzePlugins
+	})
 ]
+
+// Do you want to use bundle analyzer?
+if (process.env.ANALYZE_BUNDLE) {
+	plugins.push(new BundleAnalyzerPlugin({analyzerMode: 'static'}))
+}
 
 export default Object.assign({}, baseWebpackConfig, {
 	plugins: baseWebpackConfig.plugins.concat(plugins)
