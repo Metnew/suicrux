9c24f669dd715b7107025ec2991f7d81b2961c6b
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-14 01:04:40 +0300

    feat(server): add both https and http server in dev mode
    feat(server): add both https and http server in dev mode

--- src/server/index.dev.js
+++ src/server/index.dev.js
@@ -3,17 +3,27 @@
  * @file
  */
 import express from 'express'
+import http from 'http'
 import spdy from 'spdy'
 import chalk from 'chalk'
 // Mount our server-side code to dev server
-import server from '../../webpack_config/server'
+import server from '../../webpack_config/devServer'
 import {serverOptions as options} from './config/index.dev'
 
 const app: express$Application = express()
-const PORT: number = +process.env.PORT || 3000
+const httpsPORT: number = +process.env.PORT || 3000
+const httpPort: number = +process.env.PORT || 3000 - 443
 
 server(app)
 
-spdy.createServer(options, app).listen(PORT, () => {
-	console.log(chalk.green(`SERVER IS LISTENING ON ${PORT}`))
+spdy.createServer(options, app).listen(httpsPORT, () => {
+	console.log(
+		chalk.red(`HTTPS SERVER IS LISTENING ON https://127.0.0.1:${httpsPORT}`)
+	)
+})
+
+http.createServer(app).listen(httpPort, () => {
+	console.log(
+		chalk.green(`HTTP SERVER IS LISTENING ON http://localhost:${httpPort}`)
+	)
 })
