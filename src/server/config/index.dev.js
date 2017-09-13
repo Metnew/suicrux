7aec3a87401a498c36f9417bbd500387b832f8dd
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-14 01:04:23 +0300

    chore(server): add useful comment about "pseudossl"
    chore(server): add useful comment about "pseudossl"

--- src/server/config/index.dev.js
+++ src/server/config/index.dev.js
@@ -1,5 +1,7 @@
 import path from 'path'
 import {readFileSync} from 'fs'
+// If you want quick SSL for prototyping:
+// import pseudossl from 'pseudossl'
 
 export const serverOptions = {
 	key: readFileSync(path.join(__dirname, './ssl/server.key')),
