e4116c224c8ab1ea9f93820a08782d689b3029ef
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-13 21:25:00 +0300

    feat(server): rename HtmlComponent, remove unused vars
    feat(server): rename HtmlComponent, remove unused vars

--- src/server/ssr/index.js
+++ src/server/ssr/index.js
@@ -3,18 +3,19 @@
  * @desc
  */
 import React from 'react'
-import {readFileSync} from 'fs'
+import {readFileSync, readFile} from 'fs'
 import path from 'path'
+import _ from 'lodash'
 import {sync as globSync} from 'glob'
 import {renderToString} from 'react-dom/server'
-import Helmet from 'react-helmet'
-import createHistory from 'history/createMemoryHistory'
 import {ServerStyleSheet, StyleSheetManager} from 'styled-components'
 import {configureRootComponent, configureApp} from 'common/app'
-import IndexHTMLComponent from './IndexHTMLComponent'
-import type {Node} from 'react'
+import {addLocaleData} from 'react-intl'
+import HtmlComponent from './HtmlComponent'
 import assets from 'webpack-assets'
 import faviconsAssets from 'favicons-assets'
+
+import type {Node} from 'react'
 
 const translations = globSync('locals/*.json')
 	.map(filename => [
@@ -27,11 +28,19 @@ const translations = globSync('locals/*.json')
 		return collection
 	}, {})
 
-export default (req: Object, res: Object, next: () => void) => {
+export default (req: express$Request, res: express$Response) => {
 	const {isLoggedIn, token, lang} = req.user
 	const initialState: Object = isLoggedIn
 		? {me: {auth: {isLoggedIn, token}}}
 		: {}
+	const localeData = require('react-intl/locale-data/' + lang)
+	const i18n = {
+		lang,
+		localeData,
+		locale: lang,
+		messages: translations[lang]
+	}
+	//
 	const sheet = new ServerStyleSheet()
 	const location: string = req.url
 	const context = {}
@@ -40,6 +49,7 @@ export default (req: Object, res: Object, next: () => void) => {
 		store,
 		history,
 		routes,
+		i18n,
 		SSR: {location, context}
 	})
 	const App: string = renderToString(
@@ -54,9 +64,53 @@ export default (req: Object, res: Object, next: () => void) => {
 		assets,
 		faviconsAssets,
 		initialState: preloadedState,
-		translation: translations[lang],
+		i18n,
 		App
 	}
-	const html: string = IndexHTMLComponent(props)
+
+	Object.keys(assets).map(key => {
+		const bundle = assets[key]
+		Object.keys(bundle).map(async filetype => {
+			const filename = bundle[filetype]
+			const isJS = filetype === 'js'
+			const isCSS = filetype === 'css'
+
+			var stream = res.push(filename, {
+				status: 200, // optional
+				method: 'GET', // optional
+				request: {
+					accept: '*/*'
+				},
+				response: {
+					'content-type': isJS
+						? 'application/javascript'
+						: isCSS ? 'text/css' : 'text/html'
+				}
+			})
+
+			const readFileAsync = path => {
+				return new Promise((resolve, reject) => {
+					try {
+						readFile(path, (err, data) => {
+							if (err) {
+								reject(err)
+							}
+							resolve(data)
+						})
+					} catch (e) {
+						reject(e)
+					}
+				})
+			}
+
+			const memoizedReadFileAsync = _.memoize(readFileAsync)
+			const dataToStream = await memoizedReadFileAsync(
+				`${process.env.CLIENT_DIST_PATH}/${filename}`
+			)
+			stream.end(dataToStream)
+		})
+	})
+
+	const html: string = HtmlComponent(props)
 	res.send(html)
 }
