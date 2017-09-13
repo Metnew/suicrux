0d523f95ec10f07b65c4e701f1fc3a9b8cba6de6
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-14 01:07:19 +0300

    style(server): fix variable names
    style(server): fix variable names

--- src/server/index.js
+++ src/server/index.js
@@ -5,14 +5,14 @@
 import express from 'express'
 import spdy from 'spdy'
 import chalk from 'chalk'
-// 
+//
 import server from './server'
 import {serverOptions as options} from './config'
 const app: express$Application = express()
 const PORT: number = +process.env.PORT || 3000
 
-server(app)
+const decoratedProductionServer = server(app)
 
-spdy.createServer(options, app).listen(PORT, () => {
+spdy.createServer(options, decoratedProductionServer).listen(PORT, () => {
 	console.log(chalk.green(`SERVER IS LISTENING ON ${PORT}`))
 })
