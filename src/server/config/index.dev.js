e2d867443cc747ba8ac3b21e209fa8923cd2ebb5
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-17 14:49:39 +0300

    chore(server/config): remove index.dev, use index.js file
    chore(server/config): remove index.dev, use index.js file

--- src/server/config/index.dev.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import {readFileSync} from 'fs'
-// If you want quick SSL for prototyping:
-// import pseudossl from 'pseudossl'
-
-export const serverOptions = {
-	key: readFileSync(path.join(__dirname, './ssl/server.key')),
-	cert: readFileSync(path.join(__dirname, './ssl/server.crt'))
-}
