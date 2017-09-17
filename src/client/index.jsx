72d2f136cf423dd44a8ea5f3f4adcfa46ea96049
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-17 15:07:14 +0300

    feat(client/index): use direct imports for SUI css, add @flow
    feat(client/index): use direct imports for SUI css, add @flow

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,19 +1,43 @@
+// @flow
 // Styles
-import 'semantic-ui-css/semantic.css'
-// Fetch and promise polyfill
+// If you want full SUI CSS:
+// import 'semantic-ui-css/semantic.css'
+// If you want only some components from SUI:
+import 'semantic-ui-css/components/button.css'
+import 'semantic-ui-css/components/card.css'
+import 'semantic-ui-css/components/container.css'
+import 'semantic-ui-css/components/dimmer.css'
+import 'semantic-ui-css/components/divider.css'
+import 'semantic-ui-css/components/dropdown.css'
+import 'semantic-ui-css/components/grid.css'
+import 'semantic-ui-css/components/header.css'
+import 'semantic-ui-css/components/flag.css'
+import 'semantic-ui-css/components/form.css'
+import 'semantic-ui-css/components/item.css'
+import 'semantic-ui-css/components/icon.css'
+import 'semantic-ui-css/components/image.css'
+import 'semantic-ui-css/components/input.css'
+import 'semantic-ui-css/components/menu.css'
+import 'semantic-ui-css/components/label.css'
+import 'semantic-ui-css/components/loader.css'
+import 'semantic-ui-css/components/reset.css'
+import 'semantic-ui-css/components/sidebar.css'
+import 'semantic-ui-css/components/site.css'
+import 'semantic-ui-css/components/statistic.css'
+// promise polyfill
 import 'promise-polyfill'
-import 'isomorphic-fetch'
 // Application
 import {render} from 'react-dom'
 import {configureApp, configureRootComponent} from 'common/app'
 import {persistStore} from 'redux-persist'
+import type {GlobalState} from 'reducers'
+import type {i18nConfigObject} from 'types'
 
 if (process.env.NODE_ENV === 'production') {
 	require('common/pwa')
 } else if (process.env.NODE_ENV === 'development') {
 	// Devtools
-	// NOTE: whyDidYouUpdate package is temporary broken, waiting for a patch.
-
+	// NOTE: whyDidYouUpdate package is temporarily broken, waiting for a patch.
 	/*eslint-disable */
 	// NOTE: But if you really want to run `why-did-you-update`
 	// You can uncomment this block:
@@ -28,8 +52,7 @@ if (process.env.NODE_ENV === 'production') {
     whyDidYouUpdate(React)
   */
 	/*eslint-enable */
-	window.Perf = require('react-addons-perf')
-
+	// window.Perf = require('react-addons-perf')
 	// NOTE: a11y doesnt work with SSR and React throws warnings like:
 	// "React attempted to reuse markup in a container but the checksum was invalid"
 	// a11y adds "id" attribute to the root container (#app) of your app and triggers re-rendering
@@ -39,8 +62,8 @@ if (process.env.NODE_ENV === 'production') {
 	// a11y(React, {ReactDOM})
 }
 
-const initialState = window.__INITIAL_STATE__ || {}
-const i18n = window.__I18N__ || {}
+const initialState: GlobalState = window.__INITIAL_STATE__ || {}
+const i18n: i18nConfigObject = window.__I18N__ || {}
 // NOTE: V8 doesn't optimize `delete`
 // delete window.__INITIAL_STATE__
 const {store, routes, history} = configureApp(initialState)
