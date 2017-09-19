4063871f3bf89e40e264141227a19493e0d5cfa8
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-19 17:46:04 +0300

    fix(Footer/style): remove useless margin-top: 30px on lg screen
    fix(Footer/style): remove useless margin-top: 30px on lg screen

--- src/common/components/parts/Footer/style.jsx
+++ src/common/components/parts/Footer/style.jsx
@@ -1,18 +1,14 @@
 import styled from 'styled-components'
-import {media} from 'styles/utils'
 
 export const StyledFooter = styled.footer`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  background-color: ${props => props.theme.primaryColorLight};
-  color: ${props => props.theme.primaryTextColor};
-  height: 90px;
-  box-shadow: inset 0 0 0 0 ${props => props.theme.primaryColorDark},
-    0 2px 1px 0 ${props => props.theme.primaryColorDark};
-  ${media.lg`
-		margin-top: 30px;
-	`};
+	width: 100%;
+	display: flex;
+	align-items: center;
+	background-color: ${props => props.theme.primaryColorLight};
+	color: ${props => props.theme.primaryTextColor};
+	height: 90px;
+	box-shadow: inset 0 0 0 0 ${props => props.theme.primaryColorDark},
+		0 2px 1px 0 ${props => props.theme.primaryColorDark};
 `
 
 export const StyledFooterInner = styled.div`padding: 15px 1rem;`
