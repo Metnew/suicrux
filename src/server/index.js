a33c3e9b40183f350b767cf4f4316b2af5af33ab
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-19 17:43:38 +0300

    feat(server/index.dev): add both http and https servers in prod
    feat(server/index.dev): add both http and https servers in prod

--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,27 @@
  * @file
  */
 import express from 'express'
+import http from 'http'
 import spdy from 'spdy'
 import chalk from 'chalk'
-//
+// Mount our server-side code to server
 import server from './server'
 import {serverOptions as options} from './config'
+
 const app: express$Application = express()
-const PORT: number = +process.env.PORT || 3000
+const httpsPORT: number = +process.env.PORT || 3030
+const httpPORT: number = +process.env.PORT || 3000
 
-const decoratedProductionServer = server(app)
+server(app)
 
-spdy.createServer(options, decoratedProductionServer).listen(PORT, () => {
-	console.log(chalk.green(`SERVER IS LISTENING ON ${PORT}`))
+spdy.createServer(options, app).listen(httpsPORT, () => {
+	console.log(
+		chalk.red(`HTTPS SERVER IS LISTENING ON https://127.0.0.1:${httpsPORT}`)
+	)
+})
+
+http.createServer(app).listen(httpPORT, () => {
+	console.log(
+		chalk.green(`HTTP SERVER IS LISTENING ON http://localhost:${httpPORT}`)
+	)
 })
