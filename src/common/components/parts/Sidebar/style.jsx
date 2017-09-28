15e094f47cb6a826e59fb5c96a772af61288d7ae
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-28 19:30:07 +0300

    fix(Sidebar): use styled(Item) instead of Item.extend
    fix(Sidebar): use styled(Item) instead of Item.extend

--- src/common/components/parts/Sidebar/style.jsx
+++ src/common/components/parts/Sidebar/style.jsx
@@ -7,30 +7,29 @@ import Logo from 'components/elements/Logo'
 //   color: ${props => props.theme.primaryColorText}!important;
 // ${'' /* background-color: ${props => props.theme.accentColor}!important; */}
 export const StyledSidebar = styled(Sidebar)`
-  z-index: 111 !important;
-  display: flex !important;
-  border: none !important;
+	z-index: 111 !important;
+	display: flex !important;
+	border: none !important;
+	position: fixed !important;
 `
 //
-export const SidebarLogo = styled(Logo)`
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-`
+export const SidebarLogo = styled(Logo)`margin: 5px;`
 //
 export const SidebarLogoContainer = styled.a`
-  background-color: ${props => props.theme.primaryColorDark};
-  padding: 25px;
-  text-align: center;
-  ${media.md`
+	background-color: ${props => props.theme.accentColor};
+	color: ${props => props.theme.accentColor};
+	box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+	padding: 25px;
+	text-align: center;
+	${media.md`
     padding: 25px;
   `};
 	${media.lg`
     padding: 20px;
   `};
 `
-// Color: ${props => props.theme.primaryColorText}!important;
 export const SidebarItem = styled(Menu.Item)``
-//
-export const SidebarLogoutItem = SidebarItem.extend`
-  cursor: pointer;
-  border-top: 1px solid rgba(34, 36, 38, 0.15) !important;
+export const SidebarLogoutItem = styled(SidebarItem)`
+	cursor: pointer;
+	border-top: 1px solid rgba(34, 36, 38, 0.15) !important;
 `
