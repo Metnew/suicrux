964c82f45bf98fa25d53b8ada11132a81603e0ff
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-28 19:27:50 +0300

    feat(Header): remove language selection button
    feat(Header): remove language selection button

--- src/common/components/parts/Header/index.jsx
+++ src/common/components/parts/Header/index.jsx
@@ -2,7 +2,7 @@
  * @flow
  */
 import React, {Component} from 'react'
-import {Icon, Dropdown, Flag} from 'semantic-ui-react'
+import {Icon} from 'semantic-ui-react'
 import {isEqual} from 'lodash'
 // import {FormattedMessage} from 'react-intl'
 import {
@@ -30,41 +30,19 @@ class Header extends Component {
 
 	render () {
 		const {title, toggleSidebar, isLoggedIn, isMobile} = this.props
-		const options = [{key: 1, text: 'EN'}, {key: 2, text: 'RU'}]
-		const selectedLanguage = {
-			text: 'ru',
-			icon: 'ru'
-		}
 
 		return (
 			<Headroom>
 				<StyledHeader>
 					<HeaderInner>
 						{isLoggedIn &&
-							isMobile &&
-							<Navicon onClick={toggleSidebar}>
-								<Icon name="content" />
-							</Navicon>}
-						<PageTitle>
-							{title}
-						</PageTitle>
+							isMobile && (
+								<Navicon onClick={toggleSidebar}>
+									<Icon name="content" />
+								</Navicon>
+							)}
+						<PageTitle>{title}</PageTitle>
 						<Spacer />
-						<Dropdown
-							text={selectedLanguage.text}
-							icon={<Flag name="ru" />}
-							floating
-							labeled
-							button
-							className="icon"
-						>
-							<Dropdown.Menu>
-								<Dropdown.Header icon="globe" content="Select Language" />
-								<Dropdown.Divider />
-								<Dropdown.Item flag="us" text="EN" />
-								<Dropdown.Item flag="ru" text="RU" />
-							</Dropdown.Menu>
-						</Dropdown>
-						{/* <Flag name="" /> */}
 					</HeaderInner>
 				</StyledHeader>
 			</Headroom>
