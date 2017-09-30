f7f4be9683b7579a81b2360e32ead585a340567e
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-30 14:24:25 +0300

    feat(types): add LinkItem type, remove old types
    feat(types): add LinkItem type, remove old types

--- src/common/types/index.js
+++ src/common/types/index.js
@@ -1,33 +1,9 @@
 // @flow
-export type PostItem = {
-	title: string,
-	body: string,
-	userId: number,
-	id: number
-}
-
-export type UserItem = {
-	id: number,
-	name: string,
-	username: string,
-	email: string,
-	address: {
-		street: string,
-		suite: string,
-		city: string,
-		zipcode: string,
-		geo: {
-			lat: string,
-			lng: string
-		}
-	},
-	phone: string,
-	website: string,
-	company: {
-		name: string,
-		catchPhrase: string,
-		bs: string
-	}
+export type LinkItem = {
+	link: string,
+	header: string,
+	desc: string,
+	icon: string
 }
 
 export type RouteItem = {
