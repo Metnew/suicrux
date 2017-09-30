7c31517a30fe939b3d502238c816539db9c467fd
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-30 14:35:18 +0300

    feat(Dashboard): remove old redux stuff from Dashboard container
    feat(Dashboard): remove old redux stuff from Dashboard container

--- src/common/containers/Dashboard/index.jsx
+++ src/common/containers/Dashboard/index.jsx
@@ -1,65 +1,33 @@
 // @flow
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {Loader} from 'semantic-ui-react'
 import {Helmet} from 'react-helmet'
-//
 import DashboardComponent from './components'
-import {GET_POSTS} from 'actions/posts'
-import {getEntitiesPostsState} from 'selectors'
+
 import type {GlobalState} from 'reducers'
 
-type Props = {
-	posts: Object,
-	postsLoaded: boolean,
-	postsLoading: boolean,
-	count: number,
-	getPosts: () => void
-}
+type Props = any
 
 class Dashboard extends Component {
 	props: Props
-	componentDidMount () {
-		this.props.getPosts()
-	}
-
 	render () {
-		const {posts, postsLoaded, postsLoading, count} = this.props
 		return (
 			<div>
 				<Helmet>
 					<title>Noir:Dashboard</title>
 				</Helmet>
-				{postsLoaded
-					? <DashboardComponent
-						{...{posts, postsLoaded, postsLoading, count}}
-					/>
-					: <Loader active>Loading...</Loader>}
+				<DashboardComponent />
 			</div>
 		)
 	}
 }
 
 function mapStateToProps (state: GlobalState) {
-	const postsState = getEntitiesPostsState(state)
-	const postsLoaded = postsState.isLoaded
-	const postsLoading = postsState.isLoading
-	const posts = postsState.entities
-	const {count} = postsState
-	return {
-		posts,
-		postsLoading,
-		postsLoaded,
-		count
-	}
+	return {}
 }
 
 function mapDispatchToProps (dispatch) {
-	return {
-		getPosts () {
-			dispatch(GET_POSTS())
-		}
-	}
+	return {}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
