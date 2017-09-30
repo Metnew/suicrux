86fe771263e6cabec7d2ca5b1d8691dfdb3b7965
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-30 14:24:04 +0300

    feat(server/config): add default ssl
    feat(server/config): add default ssl

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,9 +1,22 @@
 import path from 'path'
 import {readFileSync} from 'fs'
 // If you want quick SSL for prototyping:
-// import pseudossl from 'pseudossl'
+// NOTE: you should use own SSL certs in production!
+let key = ''
+let cert = ''
+
+
+if (process.env.NODE_ENV === 'development') {
+	// REMOVE THESE LINES IF YOU HAVE VALID CERT AND KEY FOR DEVELOPMENT
+	const pseudossl = require('pseudossl')
+	key = pseudossl.key
+	cert = pseudossl.cert
+} else {
+	key = readFileSync(path.join(__dirname, './ssl/server.key'))
+	cert = readFileSync(path.join(__dirname, './ssl/server.crt'))
+}
 
 export const serverOptions = {
-	key: readFileSync(path.join(__dirname, './ssl/server.key')),
-	cert: readFileSync(path.join(__dirname, './ssl/server.crt'))
+	key,
+	cert
 }
