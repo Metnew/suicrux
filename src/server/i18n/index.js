4cafcb50d5030bb8afc961f46ceb29f27da6e0cd
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-30 14:23:40 +0300

    feat(server/i18n): watch locals dir for updates in development
    feat(server/i18n): watch locals dir for updates in development

--- src/server/i18n/index.js
+++ src/server/i18n/index.js
@@ -2,21 +2,34 @@
 import {readFileSync} from 'fs' // readFile
 import path from 'path'
 import {sync as globSync} from 'glob'
+import chokidar from 'chokidar'
 // import {addLocaleData} from 'react-intl'
 import enLocaleData from 'react-intl/locale-data/en'
 import ruLocaleData from 'react-intl/locale-data/ru'
 import type {i18nConfigObject} from 'types'
 
-const translations = globSync('locals/*.json')
-	.map((filename: string) => [
-		path.basename(filename, '.json'),
-		readFileSync(filename, 'utf8')
-	])
-	.map(([locale, file]) => [locale, JSON.parse(file)])
-	.reduce((acc, [locale, messages]) => {
-		acc[locale] = messages
-		return acc
-	}, {})
+const getTranslations = () => {
+	return globSync('locals/*.json')
+		.map((filename: string) => [
+			path.basename(filename, '.json'),
+			readFileSync(filename, 'utf8')
+		])
+		.map(([locale, file]) => [locale, JSON.parse(file)])
+		.reduce((acc, [locale, messages]) => {
+			acc[locale] = messages
+			return acc
+		}, {})
+}
+
+let translations = getTranslations()
+if (process.env.NODE_ENV === 'development') {
+	// re-update locals if they were changed (in development)
+	const watcher = chokidar.watch('locals/*.json')
+
+	watcher.on('all', () => {
+		translations = getTranslations()
+	})
+}
 
 export const defaultLanguage = 'en'
 export const supportedLanguages = ['en', 'ru']
