235b1224dad9833e5638a06c40258e289487f7bb
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-30 14:17:52 +0300

    feat(selectors): remove posts/users selector, add links selector, remove /const
    feat(selectors): remove posts/users selector, add links selector,
    remove /const

--- src/common/const/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-// Your const values live here.
-const defaultWindowInnerWidth = 1025
-export const getWindowInnerWidth = (window: Object): number => {
-	return window && window.innerWidth
-		? window.innerWidth
-		: defaultWindowInnerWidth
-}
--- src/common/selectors/index.js
+++ src/common/selectors/index.js
@@ -1,11 +1,16 @@
 // @flow
 import type {State as AuthState} from 'reducers/auth'
 import type {State as LayoutState} from 'reducers/layout'
-import type {State as EntitiesPostsState} from 'reducers/posts'
-import type {State as EntitiesUsersState} from 'reducers/users'
+import type {State as EntitiesLinksState} from 'reducers/links'
 import type {GlobalState} from 'reducers'
 
 export const getAuthState = (state: GlobalState): AuthState => state.me.auth
 export const getLayoutState = (state: GlobalState): LayoutState => state.layout
-export const getEntitiesPostsState = (state: GlobalState): EntitiesPostsState => state.entities.posts
-export const getEntitiesUsersState = (state: GlobalState): EntitiesUsersState => state.entities.users
+export const getEntitiesLinksState = (state: GlobalState): EntitiesLinksState => state.entities.links
+
+const defaultWindowInnerWidth = 1025
+export const getWindowInnerWidth = (window: Object): number => {
+	return window && window.innerWidth
+		? window.innerWidth
+		: defaultWindowInnerWidth
+}
