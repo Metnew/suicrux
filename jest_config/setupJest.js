bfff20b03e226862d7f3a2abe7be03c53c9d3def
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-10-19 00:12:42 +0300

    fix(jest_config): add process.env.BASE_API
    fix(jest_config): add process.env.BASE_API

--- jest_config/setupJest.js
+++ jest_config/setupJest.js
@@ -2,6 +2,7 @@ import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 global.fetch = require('isomorphic-fetch')
+process.env.BASE_API = 'http://localhost:3000/api/v1'
 process.env.BROWSER = false
 
 const adapter = new Adapter()
