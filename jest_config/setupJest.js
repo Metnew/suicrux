651e370c78be9d03b0cc1a7eb5bf29ea73b66d06
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-10-18 23:58:53 +0300

    fix(jest_config): remove BASE_API assign in test mode
    fix(jest_config): remove BASE_API assign in test mode

--- jest_config/setupJest.js
+++ jest_config/setupJest.js
@@ -2,7 +2,6 @@ import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 global.fetch = require('isomorphic-fetch')
-process.env.BASE_API = process.env.BASE_API || 'http://localhost:4000/api/v1'
 process.env.BROWSER = false
 
 const adapter = new Adapter()
