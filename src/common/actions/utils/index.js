ea0653ef661dfaaea277153d0df0fdc19c1f6744
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-10-30 00:36:08 +0200

    chore: comments about Awral
    chore: comments about Awral

--- src/common/actions/utils/index.js
+++ src/common/actions/utils/index.js
@@ -21,8 +21,9 @@ const beforePending = null
   Create default Awral
   Awral is not recommended for production usage now
   But it can make your work with actions even simpler.
-  NOTE:I strongly recommend you check Awral sources!
-  Awral is 880B gzipped!
+  NOTE: I strongly recommend you check Awral sources!
+  NOTE: But it's actively developed!
+  Awral is 910 bytes gzipped!
   {@link https://github.com/Metnew/awral}
 */
 export const awral = Awral.of({check, beforeCheck, afterCheck, finally: finallyAction, beforePending})
