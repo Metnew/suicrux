862d8c4cc3191fbff56fb4b6433ad2832e913506
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-03 16:38:10 +0200

    feat(styles): duplicate some styles to make SSR result nicer
    feat(styles): duplicate some styles to make SSR result nicer

--- src/common/styles/global.jsx
+++ src/common/styles/global.jsx
@@ -5,7 +5,27 @@ injectGlobal`
     box-sizing: border-box;
   }
 
-  ${'' /* body ::-webkit-scrollbar {
+  body {
+    margin: 0;
+	  padding: 0;
+	  overflow-x: hidden;
+	  min-width: 320px;
+	  background: white;
+	  font-family: 'Lato', 'Helvetica Neue', Arial, Helvetica, sans-serif;
+	  font-size: 14px;
+	  line-height: 1.4285em;
+	  color: rgba(0, 0, 0, 0.87);
+  }
+
+  .pushable {
+	  height: 100%;
+	  overflow-x: hidden;
+	  padding: 0em !important;
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  ${''/* body ::-webkit-scrollbar {
     width: 0;
   }
 
