be521a85b6c9252bddfb19fb8186849e41c93264
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-03 16:29:29 +0200

    fix(Footer): add min-height for Footer
    fix(Footer): add min-height for Footer

--- src/common/components/parts/Footer/style.jsx
+++ src/common/components/parts/Footer/style.jsx
@@ -7,6 +7,7 @@ export const StyledFooter = styled.footer`
 	background-color: ${props => props.theme.primaryColorLight};
 	color: ${props => props.theme.primaryTextColor};
 	height: 90px;
+	min-height: 90px;
 	box-shadow: inset 0 0 0 0 ${props => props.theme.primaryColorDark},
 		0 2px 1px 0 ${props => props.theme.primaryColorDark};
 `
