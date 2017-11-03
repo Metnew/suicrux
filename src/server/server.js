80b6071f11d2701287fabad7609310fd5afe169f
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-03 16:30:45 +0200

    fix(server): add FormData for server
    fix(server): add FormData for server

--- src/server/server.js
+++ src/server/server.js
@@ -5,9 +5,11 @@ import addMiddlewares from './middlewares'
 import API from './api'
 import SSR from './ssr'
 import fetch from 'isomorphic-fetch'
+import FormData from 'form-data'
 
 global.fetch = fetch
 global.window = {}
+global.FormData = FormData
 
 /**
  * Mount API, SSR and middlewares to app.
