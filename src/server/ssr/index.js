ca8ae99d84f80401aa2a2dfb28b90bca04aa3a2c
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-06 22:03:03 +0200

    feat(server): resolve assets and faviconsAssets async using `getStats()`
    feat(server): resolve assets and faviconsAssets async using `getStats()`

--- src/server/ssr/index.js
+++ src/server/ssr/index.js
@@ -11,12 +11,16 @@ import {configureRootComponent, configureApp} from 'common/app'
 import asyncBootstrapper from 'react-async-bootstrapper'
 import {AsyncComponentProvider, createAsyncContext} from 'react-async-component'
 import HTMLComponent from './HTMLComponent'
-// $FlowFixMe
-import assets from 'webpack-assets'
-// $FlowFixMe
-import faviconsAssets from 'favicons-assets'
 import getI18nData from 'server/i18n'
 import {matchPath} from 'react-router'
+import getStats from './stats'
+
+// it's better to define these objs in global scope (less memory consumption)
+let {assets, faviconsAssets} = {}
+getStats().then(result => {
+	assets = result.assets
+	faviconsAssets = result.faviconsAssets
+})
 
 export default async (req: express$Request, res: express$Response) => {
 	const {isLoggedIn, language} = req.user
