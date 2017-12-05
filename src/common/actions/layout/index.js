fbcfc67a97d4b72736bb5f504a65bab03b44dabd
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-12-05 05:00:53 +0200

    feat(actions): layout - OPEN/CLOSE_SIDEBAR -> TOGGLE_SIDEBAR
    feat(actions): layout - OPEN/CLOSE_SIDEBAR -> TOGGLE_SIDEBAR

--- src/common/actions/layout/index.js
+++ src/common/actions/layout/index.js
@@ -1,14 +1,9 @@
 // @flow
-export const UI_OPEN_SIDEBAR = 'UI_OPEN_SIDEBAR'
-export const UI_CLOSE_SIDEBAR = 'UI_CLOSE_SIDEBAR'
+export const UI_TOGGLE_SIDEBAR = 'UI_TOGGLE_SIDEBAR'
 export const UI_WINDOW_RESIZE = 'UI_WINDOW_RESIZE'
 
-export const CLOSE_SIDEBAR = () => ({
-	type: UI_CLOSE_SIDEBAR
-})
-
-export const OPEN_SIDEBAR = () => ({
-	type: UI_OPEN_SIDEBAR
+export const TOGGLE_SIDEBAR = () => ({
+	type: UI_TOGGLE_SIDEBAR
 })
 
 export const WINDOW_RESIZE = (innerWidth: number) => ({
@@ -18,8 +13,7 @@ export const WINDOW_RESIZE = (innerWidth: number) => ({
 	}
 })
 
-export type UI_OPEN_SIDEBAR_TYPE = {type: 'UI_OPEN_SIDEBAR'}
-export type UI_CLOSE_SIDEBAR_TYPE = {type: 'UI_CLOSE_SIDEBAR'}
+export type UI_TOGGLE_SIDEBAR_TYPE = {type: 'UI_TOGGLE_SIDEBAR'}
 export type UI_WINDOW_RESIZE_TYPE = {
 	type: 'UI_WINDOW_RESIZE',
 	payload: {innerWidth: number}
