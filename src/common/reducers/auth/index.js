ca3934020a911a7a8c10a2140ad9e50e0929fdae
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-12-05 05:31:23 +0200

    fix(reducers): don't handle LOGIN_AUTH_PENDING
    fix(reducers): don't handle LOGIN_AUTH_PENDING

--- src/common/reducers/auth/index.js
+++ src/common/reducers/auth/index.js
@@ -2,7 +2,6 @@
 import {isLoggedIn as hasLocalToken} from 'api/LocalStorageCookiesSvc'
 import {
 	LOGIN_AUTH_FAIL,
-	LOGIN_AUTH_PENDING,
 	LOGIN_AUTH_SUCCESS,
 	LOGOUT_AUTH_SUCCESS
 } from 'actions/auth'
@@ -10,27 +9,23 @@ import {APPLICATION_INIT} from 'actions/common'
 
 import type {
 	LOGIN_AUTH_FAIL_TYPE,
-	LOGIN_AUTH_PENDING_TYPE,
 	LOGIN_AUTH_SUCCESS_TYPE,
 	LOGOUT_AUTH_SUCCESS_TYPE
 } from 'actions/auth'
 import type {APPLICATION_INIT_TYPE} from 'actions/common'
 
 export type State = {
-	isLoggedIn: boolean,
-	errors: Object
+	isLoggedIn: boolean
 }
 
 type Action =
 	| APPLICATION_INIT_TYPE
 	| LOGIN_AUTH_FAIL_TYPE
-	| LOGIN_AUTH_PENDING_TYPE
 	| LOGIN_AUTH_SUCCESS_TYPE
 	| LOGOUT_AUTH_SUCCESS_TYPE
 
 export const initialState: State = {
-	isLoggedIn: hasLocalToken(),
-	errors: {}
+	isLoggedIn: hasLocalToken()
 }
 
 export function auth (state: State = initialState, action: Action): State {
@@ -40,27 +35,19 @@ export function auth (state: State = initialState, action: Action): State {
 	case LOGOUT_AUTH_SUCCESS: {
 		return {
 			...state,
-			isLoggedIn: false,
-			errors: {}
+			isLoggedIn: false
 		}
 	}
 	case LOGIN_AUTH_FAIL: {
-		const {errors} = action.payload
 		return {
 			...state,
-			isLoggedIn: false,
-			errors
+			isLoggedIn: false
 		}
 	}
 	case LOGIN_AUTH_SUCCESS: {
 		return {
 			...state,
 			isLoggedIn: true
-		}
-	}
-	case LOGIN_AUTH_PENDING: {
-		return {
-			...state
 		}
 	}
 	default:
