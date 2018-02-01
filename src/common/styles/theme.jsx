e0b7ce111bfa868ffd69f2b4940e34262c1e08e8
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 00:26:30 +0200

    fix(styles-theme): update accentColor
    fix(styles-theme): update accentColor

--- src/common/styles/theme.jsx
+++ src/common/styles/theme.jsx
@@ -7,7 +7,7 @@ export default {
 	primaryColor: '#1b1e2f',
 	primaryColorLight: '#303747',
 	primaryColorText: '#FFFFFF',
-	accentColor: '#e2c7c0',
+	accentColor: '#ecb8ab',
 	primaryTextColor: '#212121',
 	secondaryTextColor: '#757575',
 	dividerColor: '#BDBDBD'
