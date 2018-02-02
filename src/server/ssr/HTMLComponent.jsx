66dd88d8d15f21610d0116cfbee85a823358190d
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 16:17:08 +0200

    feat(server-ssr): simplify HtmlComponent

--- src/server/ssr/HTMLComponent.jsx
+++ src/server/ssr/HTMLComponent.jsx
@@ -1,37 +1,23 @@
-// @flow
+/// @flow
 import serealize from 'serialize-javascript'
 import _ from 'lodash'
 type args = {
+	app: string,
 	css: string,
 	asyncState: Object,
 	initialState: Object,
 	assets: Object,
-	faviconsAssets: Object,
 	i18n: Object
 }
-// renderToString
-const DLLScripts =
-	process.env.NODE_ENV === 'production'
-		? ''
-		: `
-	<script src="/polyfills.js"></script>
-	<script src="/vendor.js"></script>
-`
 
-const HTMLComponent = ({
-	css,
-	asyncState,
-	initialState,
-	assets,
-	faviconsAssets,
-	i18n
-}: args) => {
+const HTMLComponent = ({css, asyncState, initialState, assets, i18n, app}: args) => {
 	const stringifiedAsyncState: string = serealize(asyncState)
 	const stringifiedState: string = serealize(initialState)
 	const stringifiedI18N: string = serealize(i18n)
 	const wrapFuncs = {
 		css: ({path}) => `<link rel="stylesheet" href="${path}" />`,
-		js: ({path}) => `<script src="${path}" type="text/javascript"></script>`
+		js: ({path}) =>
+			`<script src="${path}" type="text/javascript" async defer></script>`
 	}
 	const assetsOrdered = ['manifest', 'vendor', 'client']
 	const getTags = assets => funcs => ext => {
@@ -50,39 +36,35 @@ const HTMLComponent = ({
 	const cssTags = getTagsFromAssets('css')
 	const jsTags = getTagsFromAssets('js')
 
-	return {
-		beforeAppTag: `<html lang="${i18n.lang}">
+	return `<html lang="${i18n.lang}">
 			<head>
 				<meta charset="utf-8" />
 				<title>Suicrux</title>
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 				<meta
 					name="description"
-					content="Advanced universal React starter built with a scale in mind."
+					content="Universal React starter. SSR/lazy-loading/i18n"
 				/>
 				<meta name="theme-color" content="#1b1e2f"/>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<base href="/" />
 				<meta name="msapplication-tap-highlight" content="no" />
 				<link rel="manifest" href="manifest.json" />
-				${faviconsAssets && faviconsAssets.html && faviconsAssets.html.join('')}
 				${css}
 				${cssTags}
 			<head>
-			<body>`,
-		afterAppTag: `
+			<body>
 			<script>window.__ASYNC_STATE__ = ${stringifiedAsyncState}</script>
 			<script>window.__INITIAL_STATE__ = ${stringifiedState}</script>
 			<script>window.__I18N__ = ${stringifiedI18N}</script>
-			${DLLScripts}
 			${jsTags}
 			<noscript>
 				You are using outdated browser. You can install modern browser here:
 				<a href="http://outdatedbrowser.com/">http://outdatedbrowser.com</a>.
 			</noscript>
+			<div id="app">${app}</div>
 			</body>
 			</html>`
-	}
 }
 
 export default HTMLComponent
