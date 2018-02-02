d19993c0a01492a3a157e3e065508a3fdcc86789
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 15:26:55 +0200

    fix(server): api prefix, remove form-data import
    fix(server): api prefix, remove form-data import

--- src/server/index.js
+++ src/server/index.js
@@ -6,22 +6,19 @@ import express from 'express'
 import 'babel-polyfill'
 //  polyfills
 import fetch from 'isomorphic-fetch'
-import FormData from 'form-data'
 // import middlewares
 import addMiddlewares from './middlewares'
 import API from './api'
 import SSR from './ssr'
 
 global.fetch = fetch
-global.window = {}
-global.FormData = FormData
 
 const app: express$Application = express()
 
 // Add global middlewares
 addMiddlewares(app)
 // Add API
-app.use(process.env.API_PREFIX, API)
+app.use('/api', API)
 // Add SSR
 app.use(SSR)
 
