52309355c9b37945ad8df7cf471867d2439a43b9
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 15:49:36 +0200

    fix(actions): formatting
    fix(actions): formatting

--- src/common/actions/common/index.js
+++ src/common/actions/common/index.js
@@ -6,8 +6,8 @@ export type LOCATION_CHANGE_TYPE = {
 		pathname: string
 	}
 }
-// XXX: DONT USE INTERNAL REDUX STUFF! e.g. @@INIT is an internal action
-// export const APP_INIT = '@@INIT'
+
+// NOTE: DON'T USE REDUX INTERNALS!
 export const APPLICATION_INIT = 'APPLICATION_INIT'
 export type APPLICATION_INIT_TYPE = {
 	type: 'APPLICATION_INIT'
