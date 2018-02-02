e229c2c7b1ad2ba19989d3bbf6ebc26c55213a5d
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 16:27:40 +0200

    fix(selectors): remove unused selectors, flowtypes
    fix(selectors): remove unused selectors, flowtypes

--- src/common/selectors/index.js
+++ src/common/selectors/index.js
@@ -1,14 +1,9 @@
 // @flow
 import {createSelector} from 'reselect'
-import type {State as AuthState} from 'reducers/auth'
-import type {State as LayoutState} from 'reducers/layout'
-import type {State as EntitiesLinksState} from 'reducers/links'
-import type {GlobalState} from 'reducers'
 
-export const getAuthState = (state: GlobalState): AuthState => state.auth
-export const getLayoutState = (state: GlobalState): LayoutState => state.layout
-export const getEntitiesLinksState = (state: GlobalState): EntitiesLinksState =>
-	state.entities.links
+export const isLoaded = state => state.fetchStatus === 'loaded'
+export const getLayoutState = state => state.layout
+export const getEntitiesLinksState = state => state.entities.links
 
 export const getWindowInnerWidth = (window: Object): number => {
 	const defaultWindowInnerWidth = 1025
