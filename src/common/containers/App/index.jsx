a3824b57552448a611d2509643879ff687ac3fe5
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 16:26:46 +0200

    fix(appcontainer): remove auth logic
    fix(appcontainer): remove auth logic

--- src/common/containers/App/index.jsx
+++ src/common/containers/App/index.jsx
@@ -4,14 +4,17 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router'
-import {push} from 'react-router-redux'
 // Import main views
-import Sidebar from 'components/parts/Sidebar'
-import Footer from 'components/parts/Footer'
-import Header from 'components/parts/Header'
+import Sidebar from 'components/Sidebar'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
 // Import actions
 import {TOGGLE_SIDEBAR, WINDOW_RESIZE} from 'actions/layout'
-import {getAuthState, getLayoutState, getWindowInnerWidth, getLayoutMobileStatuses} from 'selectors'
+import {
+	getLayoutState,
+	getWindowInnerWidth,
+	getLayoutMobileStatuses
+} from 'selectors'
 import ReactGA from 'react-ga'
 // Import styled components
 import {
@@ -21,45 +24,18 @@ import {
 	StyledDimmer
 } from './style'
 import {Container} from 'semantic-ui-react'
-import type {RouteItem} from 'types'
-import type {GlobalState} from 'reducers'
 
 type Props = {
 	children: React$Node,
-	// Routes of app passed as props in `Root`
-	routes: Array<RouteItem>,
-	// React-router `withRouter` props
 	location: any,
 	history: any,
-	// SidebarOpened can force component to re-render
 	sidebarOpened: boolean,
 	toggleSidebar: Function,
-	// IsLoggedIn can force component to re-render
-	isLoggedIn: boolean,
 	handleWindowResize: Function,
-	checkAuthLogic: Function,
-	// IsMobile can force component to re-render
 	isMobile: boolean
 }
 
-class App extends Component <Props> {
-	componentWillMount () {
-		const {isLoggedIn} = this.props
-		if (process.env.BROWSER) {
-			const {handleWindowResize} = this.props
-			handleWindowResize()
-			window.addEventListener('resize', handleWindowResize)
-		}
-		this.checkAppAuthLogic(isLoggedIn)
-	}
-
-	/**
-   * Check that user is still allowed to visit path after props changed
-   */
-	componentWillReceiveProps ({isLoggedIn}: Props) {
-		this.checkAppAuthLogic(isLoggedIn)
-	}
-
+class App extends Component<Props> {
 	componentDidMount () {
 		if (process.env.BROWSER && process.env.SENTRY_PUBLIC_DSN) {
 			const script = document.createElement('script')
@@ -69,36 +45,36 @@ class App extends Component <Props> {
 			script.onload = () => {
 				Raven.config(process.env.SENTRY_PUBLIC_DSN).install()
 			}
-			script.src = 'https://cdn.ravenjs.com/3.19.1/raven.min.js'
+			script.src = 'https://cdn.ravenjs.com/3.22.1/raven.min.js'
 			document.body.appendChild(script)
 		}
 
 		if (process.env.BROWSER && process.env.GA_ID) {
+			const {location: {search, pathname}} = this.props
 			ReactGA.initialize(process.env.GA_ID)
+			ReactGA.pageview(pathname + search)
 		}
 	}
 
-	/**
-     * Check that user is allowed to visit route
-     * @param  {Boolean} isLoggedIn state.auth.me.isLoggedIn, current prop
-     * @return {Void}
-     */
-	checkAppAuthLogic (isLoggedIn: boolean) {
-		const {pathname} = this.props.location
-		this.props.checkAuthLogic(pathname, isLoggedIn)
+	componentWillReceiveProps (nextProps) {
+		if (process.env.BROWSER && process.env.GA_ID) {
+			const {location: {search, pathname}} = this.props
+			ReactGA.pageview(pathname + search)
+		}
+	}
+
+	componentWillMount () {
+		if (process.env.BROWSER) {
+			const {handleWindowResize} = this.props
+			handleWindowResize()
+			window.addEventListener('resize', handleWindowResize)
+		}
 	}
 
 	render () {
-		const {
-			children,
-			sidebarOpened,
-			toggleSidebar,
-			isLoggedIn,
-			isMobile
-		} = this.props
-
+		const {children, sidebarOpened, toggleSidebar, isMobile} = this.props
 		const dimmerProps = {
-			active: isLoggedIn && sidebarOpened,
+			active: sidebarOpened,
 			page: true,
 			onClick: toggleSidebar
 		}
@@ -110,11 +86,8 @@ class App extends Component <Props> {
 		return (
 			<PageLayout>
 				<SidebarSemanticPushable>
-					{isLoggedIn && <Sidebar />}
-					<SidebarSemanticPusherStyled
-						isloggedin={isLoggedIn ? '1' : ''}
-						ismobile={isMobile ? '1' : ''}
-					>
+					<Sidebar />
+					<SidebarSemanticPusherStyled ismobile={isMobile ? '1' : ''}>
 						<StyledDimmer {...dimmerProps} />
 						<Header />
 						<div className="main-layout">
@@ -130,15 +103,13 @@ class App extends Component <Props> {
 	}
 }
 
-function mapStateToProps (state: GlobalState) {
+function mapStateToProps (state) {
 	const {sidebarOpened} = getLayoutState(state)
-	const {isLoggedIn} = getAuthState(state)
 	const {isMobile} = getLayoutMobileStatuses(state)
 
 	return {
 		sidebarOpened,
-		isMobile,
-		isLoggedIn
+		isMobile
 	}
 }
 
@@ -147,21 +118,6 @@ function mapDispatchToProps (dispatch) {
 	return {
 		toggleSidebar () {
 			dispatch(TOGGLE_SIDEBAR())
-		},
-		/**
-         * Immediately push to homePath('/'), if user is logged.
-         * Can be used for other auth logic checks.
-         * Useful, because we don't need to dispatch `push(homePath)` action
-         * from `Login` container after LOGIN_AUTH_SUCCESS action
-         * @param  {String}  path       [current location path]
-         * @param  {Boolean} isLoggedIn [is user logged in?]
-         */
-		checkAuthLogic (path: string, isLoggedIn: boolean) {
-			const authPath = '/auth'
-			const homePath = '/'
-			if (isLoggedIn && path === authPath) {
-				dispatch(push(homePath))
-			}
 		},
 		handleWindowResize () {
 			clearTimeout(resizer)
