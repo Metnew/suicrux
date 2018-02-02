c10dfe1dfbdde0974bf9ab120667a142cb2dfc70
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 16:27:24 +0200

    fix(link-item): pass props w/o object wrapper
    fix(link-item): pass props w/o object wrapper

--- src/common/containers/Links/components/LinkItem.jsx
+++ src/common/containers/Links/components/LinkItem.jsx
@@ -2,19 +2,24 @@
 import React from 'react'
 import {List} from 'semantic-ui-react'
 
-import type {LinkItem as LinkItemType} from 'types'
 type Props = {
-	item: LinkItemType
+	link: string,
+	header: string,
+	desc: string,
+	icon: string
 }
 
-const LinkItem = ({item}: Props) => {
-	const {header, icon, desc, link} = item
+const LinkItem = ({header, icon, desc, link}: Props) => {
 	return (
 		<List.Item>
 			<List.Icon name={icon} size="large" verticalAlign="middle" />
 			<List.Content>
-				<List.Header as="a" href={link}>{header}</List.Header>
-				<List.Description as="a" href={link}>{desc}</List.Description>
+				<List.Header as="a" href={link}>
+					{header}
+				</List.Header>
+				<List.Description as="a" href={link}>
+					{desc}
+				</List.Description>
 			</List.Content>
 		</List.Item>
 	)
