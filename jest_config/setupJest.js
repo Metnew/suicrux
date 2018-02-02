89b02a6967f5bdbbeec8d177a2837a6e7783191b
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 15:24:09 +0200

    fix(jest_config): remove BASE_API handling
    fix(jest_config): remove BASE_API handling

--- jest_config/setupJest.js
+++ jest_config/setupJest.js
@@ -3,10 +3,8 @@ import Adapter from 'enzyme-adapter-react-16'
 
 global.fetch = require('isomorphic-fetch')
 /** {@link: https://reactjs.org/blog/2017/09/26/react-v16.0.html#javascript-environment-requirements} */
-global.requestAnimationFrame = function (cb) {
-	setTimeout(cb, 0)
-}
-process.env.BASE_API = process.env.BASE_API || 'http://localhost:3000/api/v1'
+global.requestAnimationFrame = cb => setTimeout(cb, 0)
+
 process.env.BROWSER = false
 
 const adapter = new Adapter()
