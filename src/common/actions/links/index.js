ea000470ddbb731b1a5b14cf6dfdde88aad75442
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-02 16:29:48 +0200

    fix(actions): remove awral, use promise-middleware
    fix(actions): remove awral, use promise-middleware

--- src/common/actions/links/index.js
+++ src/common/actions/links/index.js
@@ -1,7 +1,5 @@
 // @flow
-import {awral} from 'actions/utils'
 import {getLinksAPI} from 'api/LinksSvc'
-import type {LinkItem} from 'types'
 // Define action types
 export const GET_LINKS_SUCCESS = 'GET_LINKS_SUCCESS'
 export const GET_LINKS_FAIL = 'GET_LINKS_FAIL'
@@ -10,7 +8,7 @@ export const GET_LINKS_PENDING = 'GET_LINKS_PENDING'
 export type GET_LINKS_SUCCESS_TYPE = {
 	type: 'GET_LINKS_SUCCESS',
 	meta: any,
-	payload: Array<LinkItem>
+	payload: any
 }
 
 export type GET_LINKS_FAIL_TYPE = {
@@ -27,11 +25,7 @@ export type GET_LINKS_PENDING_TYPE = {
 	meta: any
 }
 
-/**
-  Awral is not recommended for production usage now
-  But it can make your work with actions even simpler.
-  NOTE: I strongly recommend you check Awral's sources!
-  Awral is 910 bytes gzipped!
-  {@link https://github.com/Metnew/awral}
-*/
-export const GET_LINKS = awral(getLinksAPI)('GET_LINKS')
+export const GET_LINKS = () => ({
+	type: 'GET_LINKS',
+	payload: getLinksAPI()
+})
