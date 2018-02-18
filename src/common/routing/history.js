209164b833113bc3fc84af4adfa9821af768e4ad
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-18 15:50:05 +0200

    fix: remove unnecessary components/actions/reducers

--- src/common/routing/history.js
+++
@@ -1,5 +0,0 @@
-// @flow
-import {createBrowserHistory, createMemoryHistory} from 'history'
-const selectedHistory = process.env.BROWSER ? createBrowserHistory : createMemoryHistory
-
-export default selectedHistory()
