5b78ac3024091d137c56d04803ac605a9abbe48f
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-18 16:06:05 +0200

    fix(reducers): update rootReducer creation
    fix(reducers): update rootReducer creation

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -2,24 +2,14 @@
 import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
 
-import type {State as AuthState} from 'reducers/auth'
-import type {State as LayoutState} from 'reducers/layout'
-import type {State as EntitiesLinksState} from 'reducers/links'
-
 import {layout} from './layout'
 import {links} from './links'
-import {auth} from './auth'
 
 // Root reducer
 export default combineReducers({
 	layout,
-	me: combineReducers({auth}),
 	entities: combineReducers({
 		links
 	}),
 	routing: routerReducer
 })
-
-export type GlobalState = {layout: LayoutState} & {me: {auth: AuthState}} & {
-		entities: {links: EntitiesLinksState}
-	}
