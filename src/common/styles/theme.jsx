14ed15dddbffd8fd70060ddcbac9c529d30a8e15
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-18 16:02:29 +0200

    fix(styles): make theme less complex
    fix(styles): make theme less complex

--- src/common/styles/theme.jsx
+++ src/common/styles/theme.jsx
@@ -1,14 +1,5 @@
-/**
- * You know where to get a new material palette, right?:
- * {@link https://www.materialpalette.com/}
- */
 export default {
-	primaryColorDark: '#0e1319',
-	primaryColor: '#1b1e2f',
-	primaryColorLight: '#303747',
-	primaryColorText: '#FFFFFF',
-	accentColor: '#e2c7c0',
-	primaryTextColor: '#212121',
-	secondaryTextColor: '#757575',
-	dividerColor: '#BDBDBD'
+	white: '#FFF',
+	black: '#000',
+	navy: '#20222c'
 }
