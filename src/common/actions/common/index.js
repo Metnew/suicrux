79111604053c74c1100598723af614b773baf430
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-18 15:57:51 +0200

    fix(actions-common): remove flowtypes

--- src/common/actions/common/index.js
+++ src/common/actions/common/index.js
@@ -1,14 +1,3 @@
 // @flow
 export const LOCATION_CHANGE = '@@router/LOCATION_CHANGE'
-export type LOCATION_CHANGE_TYPE = {
-	type: '@@router/LOCATION_CHANGE',
-	payload: {
-		pathname: string
-	}
-}
-// XXX: DONT USE INTERNAL REDUX STUFF! e.g. @@INIT is an internal action
-// export const APP_INIT = '@@INIT'
 export const APPLICATION_INIT = 'APPLICATION_INIT'
-export type APPLICATION_INIT_TYPE = {
-	type: 'APPLICATION_INIT'
-}
