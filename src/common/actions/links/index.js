5a7d2624cbb6d8877a35253199249af5f28f1847
Vladimir Metnev <vladimirmetnew@gmail.com>
2018-02-19 02:39:54 +0200

    fix(actions-links): don't handle REJECTED action, update tests
    fix(actions-links): don't handle REJECTED action, update tests

--- src/common/actions/links/index.js
+++ src/common/actions/links/index.js
@@ -2,7 +2,8 @@
 import {getLinksAPI} from 'api/LinksSvc'
 // Define action types
 export const GET_LINKS_FULFILLED = 'GET_LINKS_FULFILLED'
-export const GET_LINKS_REJECTED = 'GET_LINKS_REJECTED'
+// GET_LINKS_REJECTED isn't handled in the boilerplate.
+// export const GET_LINKS_REJECTED = 'GET_LINKS_REJECTED'
 export const GET_LINKS_PENDING = 'GET_LINKS_PENDING'
 
 export const GET_LINKS = () => ({
